8560ea797e02e593197e3b6d2c93f118197c1186
Jasintha <jasinthad@gmail.com>
2014-04-09 14:59:05 +0530

    adding avro datam write pattern to format the output data

--- datamapper/org.wso2.datamapper.engine/src/main/java/org/wso2/datamapper/engine/sample/MapperMain.java
+++ datamapper/org.wso2.datamapper.engine/src/main/java/org/wso2/datamapper/engine/sample/MapperMain.java
@@ -1,5 +1,6 @@
 package org.wso2.datamapper.engine.sample;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,17 +8,18 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.DatumWriter;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMXMLBuilderFactory;
 import org.apache.axiom.om.OMXMLParserWrapper;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.wso2.datamapper.engine.core.MappingHandler;
 import org.wso2.datamapper.engine.core.MappingResourceLoader;
-import org.wso2.datamapper.engine.utils.OutputJsonBuilder;
+import org.wso2.datamapper.engine.core.writer.DummyEncoder;
+import org.wso2.datamapper.engine.core.writer.WriterRegistry;
 /**
  * This is a Test class will be removed in the future
- * @author jasintha
  *
  */
 public class MapperMain {
@@ -36,14 +38,38 @@ public class MapperMain {
 		    OMElement documentElement = builder.getDocumentElement();
 
 		    GenericRecord result = MappingHandler.doMap(documentElement,configModel);			
+		 
 			
+	       ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			
-			OutputJsonBuilder outJsonBuilder = new OutputJsonBuilder();
-			JSONObject resultJson = outJsonBuilder.getOutPut(result, configModel.getOutputRootelement());
-		
-			System.out.println(resultJson.toString());  
-			inStream.close();
+			//CSV : text/csv
+			//XML : application/xml
+		    
+		    DatumWriter<GenericRecord> writer = WriterRegistry.getInstance().get("text/csv").newInstance(); 
+		    writer.setSchema(result.getSchema());
+		    Encoder encoder =  new DummyEncoder(baos);
+		 
+		    writer.write(result, encoder);
+		    encoder.flush();
 
+		    //Print CSV
+		    System.out.println("-- CSV -- \n");
+			System.out.println(baos.toString());
+			
+			
+			baos = new ByteArrayOutputStream();
+		    writer = WriterRegistry.getInstance().get("application/xml").newInstance(); 
+		    writer.setSchema(result.getSchema());
+		    encoder =  new DummyEncoder(baos);
+		 
+		    writer.write(result, encoder);
+		    encoder.flush();
+
+		    //Print XML
+		    System.out.println("\n\n-- XML -- \n");
+			System.out.println(baos.toString());
+			
+			inStream.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

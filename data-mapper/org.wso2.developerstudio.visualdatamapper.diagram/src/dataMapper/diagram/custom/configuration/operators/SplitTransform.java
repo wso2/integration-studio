4600f0c2ab1ccf89764765c5824c6fccb04abe5c
lsdeva <lali.pera@gmail.com>
2014-04-16 17:35:07 +0530

    implement code generation class for split operator

---
+++ data-mapper/org.wso2.developerstudio.visualdatamapper.diagram/src/dataMapper/diagram/custom/configuration/operators/SplitTransform.java
@@ -0,0 +1,57 @@
+package dataMapper.diagram.custom.configuration.operators;
+
+import java.util.ArrayList;
+
+import org.eclipse.emf.common.util.EList;
+
+import dataMapper.Element;
+import dataMapper.Operator;
+import dataMapper.OperatorRightConnector;
+import dataMapper.SchemaDataType;
+import dataMapper.Split;
+import dataMapper.TreeNode;
+import dataMapper.diagram.custom.configuration.function.AssignmentStatement;
+
+public class SplitTransform implements OperatorsTransformer {
+
+	@Override
+	public AssignmentStatement transform(Operator operator) {
+		AssignmentStatement assign = new AssignmentStatement();
+		StringBuilder statement = new StringBuilder();
+		ArrayList<Element> splitOutputs = getOutputElements(operator);
+		Element splitInput = getInputElement(operator);
+		String index = "";
+		if(splitInput.getFieldParent().getSchemaDataType().equals(SchemaDataType.ARRAY)){
+			index = "[i]";
+		}
+		Split split = (Split) operator;
+		int i=0;
+		for(Element output : splitOutputs){
+
+				statement.append(output.getFieldParent().getName()+index+"."+output.getName()+ " = " + splitInput.getFieldParent().getName()+index+"."+splitInput.getName()+".split(\""+split.getDelimiter()+"\")"+"["+i+"];\n");
+
+			i++;
+		}
+		
+		assign.setStatement(statement.toString());
+		return assign;
+	}
+	
+	private Element getInputElement( Operator operator) {
+		return operator.getBasicContainer().getLeftContainer().getLeftConnectors().get(0).getInNode().getIncomingLink().get(0).getOutNode().getElementParent();
+	}
+	@Override
+	public TreeNode getOutputElementParent(Operator operator) {
+		return getOutputElements(operator).get(0).getFieldParent();
+	}
+	
+	private ArrayList<Element> getOutputElements(Operator operator) {
+		EList<OperatorRightConnector> rightConnectors = operator.getBasicContainer().getRightContainer().getRightConnectors();
+		ArrayList<Element> elementList = new ArrayList<Element>();
+		for(OperatorRightConnector connector : rightConnectors){
+			elementList.add(connector.getOutNode().getOutgoingLink().get(0).getInNode().getElementParent());
+		}
+		return elementList;
+	}
+
+}

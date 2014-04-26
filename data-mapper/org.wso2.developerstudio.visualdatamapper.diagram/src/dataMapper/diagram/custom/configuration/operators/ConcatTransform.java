a93e2f0eb3aa361a5a0cd82c6cd1f61124eccb42
lsdeva <lali.pera@gmail.com>
2014-04-26 22:02:54 +0530

    change concat transformer, config with tree hierachy

--- data-mapper/org.wso2.developerstudio.visualdatamapper.diagram/src/dataMapper/diagram/custom/configuration/operators/ConcatTransform.java
+++ data-mapper/org.wso2.developerstudio.visualdatamapper.diagram/src/dataMapper/diagram/custom/configuration/operators/ConcatTransform.java
@@ -35,16 +35,17 @@ public class ConcatTransform implements OperatorsTransformer{
 	
 	@Override
 	public AssignmentStatement transform(Operator operator) {
+		Concat concat = (Concat) operator;
 		ArrayList<Element> concatInput = getInputElements(operator);
 		String index = "";
 		if(concatInput.get(0).getFieldParent().getSchemaDataType().equals(SchemaDataType.ARRAY)){
 			index = INDEX;
 		}
-		
+		TreeNode inputRootForMap = getInputElementParent(operator);
 		AssignmentStatement assign = new AssignmentStatement();
 		
 		String outputParentName = getOutputElementParent(operator).getName();
-		String inputParentName = concatInput.get(0).getFieldParent().getName();
+		String inputParentName = inputRootForMap.getName();
 		
 		/*
 		 * If input parameter and output parameter names are identical,
@@ -55,15 +56,14 @@ public class ConcatTransform implements OperatorsTransformer{
 		}
 		
 		StringBuilder statement = new StringBuilder(outputParentName + index + "." + getOutputElement(operator).getName() + "=");
-		statement.append(concatInput.get(0).getFieldParent().getName() + index + "." + concatInput.get(0).getName());
+		statement.append(getTreeHierarchy(concatInput.get(0).getFieldParent(), inputRootForMap)+ "." + concatInput.get(0).getName());
 		concatInput.remove(0);
-		Concat concat = (Concat) operator;
 		
 		for(Element element : concatInput){
 			if(concat.getDelimiter() != null && !concat.getDelimiter().equalsIgnoreCase("")){
-				statement.append(".concat("+"\""+concat.getDelimiter()+"\"+"+element.getFieldParent().getName()+index+"."+element.getName()+")");
+				statement.append(".concat("+"\""+concat.getDelimiter()+"\"+"+getTreeHierarchy(element.getFieldParent(), inputRootForMap)+"."+element.getName()+")");
 			}else{				
-				statement.append(".concat("+element.getFieldParent().getName()+index+"."+element.getName()+")");
+				statement.append(".concat("+getTreeHierarchy(element.getFieldParent(), inputRootForMap)+"."+element.getName()+")");
 			}
 		}
 		statement.append(";");
@@ -85,6 +85,32 @@ public class ConcatTransform implements OperatorsTransformer{
 		return getOutputElement(operator).getFieldParent();
 	}
 	
+	@Override
+	public TreeNode getInputElementParent(Operator operator){
+		ArrayList<Element> inputElements = getInputElements(operator);
+		TreeNode highestParent = null;
+		for (Element element : inputElements){
+			if (element != null) {
+				if (highestParent != null) {
+					if (highestParent.getLevel() >= element.getFieldParent().getLevel()) {
+						highestParent = element.getFieldParent();
+					}
+				} else {
+					highestParent = element.getFieldParent();
+				}
+			}
+		}
+		
+		if(getOutputElement(operator).getFieldParent().getSchemaDataType().equals(SchemaDataType.ARRAY) && !(highestParent.getSchemaDataType().equals(SchemaDataType.ARRAY))){
+			while(highestParent.getFieldParent() != null && !(highestParent.getSchemaDataType().equals(SchemaDataType.ARRAY))){
+				highestParent = highestParent.getFieldParent();
+			}
+		}
+		
+		return highestParent;
+		
+	}
+	
 	/**
 	 * list of input elements needs to create assignment statements 
 	 * @param operator concat operator object
@@ -94,9 +120,32 @@ public class ConcatTransform implements OperatorsTransformer{
 		EList<OperatorLeftConnector> leftConnectors = operator.getBasicContainer().getLeftContainer().getLeftConnectors();
 		ArrayList<Element> elementList = new ArrayList<Element>();
 		for(OperatorLeftConnector connector : leftConnectors){
-			elementList.add(connector.getInNode().getIncomingLink().get(0).getOutNode().getElementParent());
+			if(connector.getInNode().getIncomingLink().size() != 0){
+				elementList.add(connector.getInNode().getIncomingLink().get(0).getOutNode().getElementParent());
+			}
 		}
 		return elementList;
+	}
+	
+	
+	private String getTreeHierarchy(TreeNode tree, TreeNode parent) {
+		StringBuilder hierarchy = new StringBuilder();
+
+		while (!(tree.equals(parent))) {
+			hierarchy.insert(0, tree.getName());
+			hierarchy.insert(0, ".");
+			tree = tree.getFieldParent();
+		}
+		
+		if(tree.getSchemaDataType().equals(SchemaDataType.ARRAY)){
+			hierarchy.insert(0, (tree.getName()+INDEX));
+		}
+		else {
+			hierarchy.insert(0, tree.getName());
+		}
+
+		return hierarchy.toString();
+
 	}
 
 

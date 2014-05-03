5ebc0c911c0843d434e3fa6eaec9ba4d82a17337
lsdeva <lali.pera@gmail.com>
2014-05-04 02:34:56 +0530

    concat tranformer extend from  manyToOne class

---
+++ data-mapper/org.wso2.developerstudio.visualdatamapper.diagram/src/org/wso2/developerstudio/datamapper/diagram/custom/configuration/operators/ManyToOneTransformer.java
@@ -0,0 +1,33 @@
+package org.wso2.developerstudio.datamapper.diagram.custom.configuration.operators;
+
+import org.wso2.developerstudio.datamapper.Operator;
+import org.wso2.developerstudio.datamapper.TreeNode;
+import org.wso2.developerstudio.datamapper.diagram.custom.configuration.function.AssignmentStatement;
+
+public class ManyToOneTransformer implements OperatorsTransformer{
+
+	@Override
+	public AssignmentStatement transform(Operator operator) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode getOutputElementParent(Operator operator) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode getInputElementParent(Operator operator) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String trasnform(String statement, Operator operator, Operator nextOperator) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

87ac2bad0d944742a13d9c30b6309409926e793d
tikaa <wnthksnrth@gmail.com>
2014-12-09 16:52:30 +0530

    refactoring changes

--- esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/keyhandlers/ESBKeyHandler.java
+++ esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/keyhandlers/ESBKeyHandler.java
@@ -38,29 +38,29 @@ import org.wso2.developerstudio.eclipse.logging.core.Logger;
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class ESBKeyHandler extends AbstractHandler {
-	private static final String ESBPROJECT_COMMANDS_NEW_MSG_STORE = "esbproject.commands.new.msgStore";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_TASK_WIZARD = "org.wso2.developerstudio.eclipse.artifact.task.wizard";
-	private static final String ESBPROJECT_COMMANDS_SAMPLE_COMMAND = "esbproject.commands.sampleCommand";
-	private static final String ESBPROJECT_COMMANDS_NEW_SCHED_TASK = "esbproject.commands.new.schedTask";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_SYNAPSEARTIFACT = "org.wso2.developerstudio.eclipse.artifact.synapseartifact";
-	private static final String ESBPROJECT_COMMANDS_NEW_SYNAPSE = "esbproject.commands.new.synapse";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWTEMPLATE = "org.wso2.developerstudio.eclipse.artifact.newtemplate";
-	private static final String ESBPROJECT_COMMANDS_NEW_TEMPLATE = "esbproject.commands.new.template";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_SYNAPSE_API_WIZARD = "org.wso2.developerstudio.eclipse.artifact.synapse.api.wizard";
-	private static final String ESBPROJECT_COMMANDS_NEW_RST_API = "esbproject.commands.new.rstAPI";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWSEQUENCEARTIFACT = "org.wso2.developerstudio.eclipse.artifact.newsequenceartifact";
-	private static final String ESBPROJECT_COMMANDS_NEW_SE_QUENCE = "esbproject.commands.new.seQuence";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWPROXYSERVICE = "org.wso2.developerstudio.eclipse.artifact.newproxyservice";
-	private static final String ESBPROJECT_COMMANDS_NEW_PROX_SERVICE = "esbproject.commands.new.proxService";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_MESSAGEPROCESSOR_WIZARD = "org.wso2.developerstudio.eclipse.artifact.messageprocessor.wizard";
-	private static final String ESBPROJECT_COMMANDS_NEW_MSG_PROC = "esbproject.commands.new.msgProc";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWMEDIATORARTIFACT = "org.wso2.developerstudio.eclipse.artifact.newmediatorartifact";
-	private static final String ESBPROJECT_COMMANDS_NEW_MEDIATOR_PRO = "esbproject.commands.new.mediatorPro";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWENDPOINTARTIFACT = "org.wso2.developerstudio.eclipse.artifact.newendpointartifact";
-	private static final String ESBPROJECT_COMMANDS_NEW_END_POINT = "esbproject.commands.new.endPoint";
-	private static final String ESBPROJECT_COMMANDS_NEW_LCL_ENTRY = "esbproject.commands.new.lclEntry";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWLOCALENTRY = "org.wso2.developerstudio.eclipse.artifact.newlocalentry";
-	private static final String ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_MESSAGESTORE_WIZARD = "org.wso2.developerstudio.eclipse.artifact.messagestore.wizard";
+	private static final String NEW_MSG_STORE_COMMAND = "esbproject.commands.new.msgStore";
+	private static final String ARTIFACT_TASK_WIZARD = "org.wso2.developerstudio.eclipse.artifact.task.wizard";
+	private static final String SAMPLE_COMMAND = "esbproject.commands.sampleCommand";
+	private static final String NEW_SCHED_TASK_COMMAND = "esbproject.commands.new.schedTask";
+	private static final String ARTIFACT_SYNAPSEARTIFACT = "org.wso2.developerstudio.eclipse.artifact.synapseartifact";
+	private static final String NEW_SYNAPSE_COMMAND = "esbproject.commands.new.synapse";
+	private static final String ARTIFACT_NEWTEMPLATE = "org.wso2.developerstudio.eclipse.artifact.newtemplate";
+	private static final String NEW_TEMPLATE_COMMAND = "esbproject.commands.new.template";
+	private static final String SYNAPSE_API_WIZARD = "org.wso2.developerstudio.eclipse.artifact.synapse.api.wizard";
+	private static final String NEW_RST_API_COMMAND = "esbproject.commands.new.rstAPI";
+	private static final String NEW_SEQUENCE_ARTIFACT = "org.wso2.developerstudio.eclipse.artifact.newsequenceartifact";
+	private static final String NEW_SE_QUENCE_COMMAND = "esbproject.commands.new.seQuence";
+	private static final String NEW_PROXY_SERVICE = "org.wso2.developerstudio.eclipse.artifact.newproxyservice";
+	private static final String NEW_PROX_SERVICE_COMMAND = "esbproject.commands.new.proxService";
+	private static final String MESSAGE_PROCESSOR_WIZARD = "org.wso2.developerstudio.eclipse.artifact.messageprocessor.wizard";
+	private static final String NEW_MSG_PROC_COMMAND = "esbproject.commands.new.msgProc";
+	private static final String NEW_MEDIATOR_ARTIFACT = "org.wso2.developerstudio.eclipse.artifact.newmediatorartifact";
+	private static final String NEW_MEDIATOR_PRO_COMMAND = "esbproject.commands.new.mediatorPro";
+	private static final String NEW_ENDPOINT_ARTIFACT = "org.wso2.developerstudio.eclipse.artifact.newendpointartifact";
+	private static final String NEW_END_POINT_COMMAND = "esbproject.commands.new.endPoint";
+	private static final String NEW_LCL_ENTRY_COMMAND = "esbproject.commands.new.lclEntry";
+	private static final String NEW_LOCAL_ENTRY = "org.wso2.developerstudio.eclipse.artifact.newlocalentry";
+	private static final String MESSAGE_STORE_WIZARD = "org.wso2.developerstudio.eclipse.artifact.messagestore.wizard";
 
 	protected final IDeveloperStudioLog log = Logger.getLog(Activator.PLUGIN_ID);
 
@@ -75,29 +75,29 @@ public class ESBKeyHandler extends AbstractHandler {
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		if (ESBPROJECT_COMMANDS_NEW_MSG_STORE.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_MESSAGESTORE_WIZARD);
-		} else if (ESBPROJECT_COMMANDS_NEW_LCL_ENTRY.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWLOCALENTRY);
-		} else if (ESBPROJECT_COMMANDS_NEW_END_POINT.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWENDPOINTARTIFACT);
-		} else if (ESBPROJECT_COMMANDS_NEW_MEDIATOR_PRO.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWMEDIATORARTIFACT);
-		} else if (ESBPROJECT_COMMANDS_NEW_MSG_PROC.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_MESSAGEPROCESSOR_WIZARD);
-		} else if (ESBPROJECT_COMMANDS_NEW_PROX_SERVICE.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWPROXYSERVICE);
-		} else if (ESBPROJECT_COMMANDS_NEW_SE_QUENCE.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWSEQUENCEARTIFACT);
-		} else if (ESBPROJECT_COMMANDS_NEW_RST_API.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_SYNAPSE_API_WIZARD);
-		} else if (ESBPROJECT_COMMANDS_NEW_TEMPLATE.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_NEWTEMPLATE);
-		} else if (ESBPROJECT_COMMANDS_NEW_SYNAPSE.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_SYNAPSEARTIFACT);
-		} else if (ESBPROJECT_COMMANDS_NEW_SCHED_TASK.equals(event.getCommand().getId())) {
-			openWizard(ORG_WSO2_DEVELOPERSTUDIO_ECLIPSE_ARTIFACT_TASK_WIZARD);
-		} else if (ESBPROJECT_COMMANDS_SAMPLE_COMMAND.equals(event.getCommand().getId())) {
+		if (NEW_MSG_STORE_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(MESSAGE_STORE_WIZARD);
+		} else if (NEW_LCL_ENTRY_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(NEW_LOCAL_ENTRY);
+		} else if (NEW_END_POINT_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(NEW_ENDPOINT_ARTIFACT);
+		} else if (NEW_MEDIATOR_PRO_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(NEW_MEDIATOR_ARTIFACT);
+		} else if (NEW_MSG_PROC_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(MESSAGE_PROCESSOR_WIZARD);
+		} else if (NEW_PROX_SERVICE_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(NEW_PROXY_SERVICE);
+		} else if (NEW_SE_QUENCE_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(NEW_SEQUENCE_ARTIFACT);
+		} else if (NEW_RST_API_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(SYNAPSE_API_WIZARD);
+		} else if (NEW_TEMPLATE_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(ARTIFACT_NEWTEMPLATE);
+		} else if (NEW_SYNAPSE_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(ARTIFACT_SYNAPSEARTIFACT);
+		} else if (NEW_SCHED_TASK_COMMAND.equals(event.getCommand().getId())) {
+			openWizard(ARTIFACT_TASK_WIZARD);
+		} else if (SAMPLE_COMMAND.equals(event.getCommand().getId())) {
 			// EsbMultiPageEditor x;
 			IEditorPart editorReferences[] = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getEditors();
 			for (int i = 0; i < editorReferences.length; i++) {

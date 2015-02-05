f9e51f02504dd860bf7f497decc357d2092ae7cc
rajeevanWSO2 <rajeevanv89@gmail.com>
2015-02-05 13:07:11 +0530

    Fixes for Issue TOOLS-2378

--- common/org.wso2.developerstudio.eclipse.utils/src/org/wso2/developerstudio/eclipse/utils/wst/Axis2ServiceUtils.java
+++ common/org.wso2.developerstudio.eclipse.utils/src/org/wso2/developerstudio/eclipse/utils/wst/Axis2ServiceUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2010 - 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,11 +16,7 @@
 
 package org.wso2.developerstudio.eclipse.utils.wst;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,166 +35,301 @@ import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
+import org.wso2.developerstudio.eclipse.logging.core.Logger;
+import org.wso2.developerstudio.eclipse.utils.Activator;
+import org.wso2.developerstudio.eclipse.utils.exception.Axis2ServiceUtilsException;
 import org.wso2.developerstudio.eclipse.utils.file.FileUtils;
 import org.xml.sax.SAXException;
 
+/**
+ * This class provides static utility methods for Axis2 service project management tasks.
+ * Such as Axis2 services.xml file manipulation, data extraction etc.
+ */
 public class Axis2ServiceUtils {
-	public static Map<IFolder,IProject> getServiceFolders() throws CoreException{
-		HashMap<IFolder, IProject> map = new HashMap<IFolder, IProject>();
-		for(IProject project:ResourcesPlugin.getWorkspace().getRoot().getProjects()){
-			if (WebUtils.isDynamicWebProject(project)){
-				final IFolder servicesFolder;
-                try {
-	                servicesFolder = WebUtils.getAxis2WebContainerWEB_INFServicesFolderPath(project);
-                } catch (Exception e1) {
-                	//some error occured meaning that this isn't a project type we are looking for
-	                continue;
-                }
-				final List<IFolder> folders=new ArrayList<IFolder>();
-				try {
-	                if (servicesFolder.exists()) {
-	                    servicesFolder.accept(new IResourceVisitor() {
-		                    public boolean visit(IResource resource) throws CoreException {
-			                    if (resource.getLocation().toOSString()
-			                            .equals(servicesFolder.getLocation().toOSString())) {
-				                    return true;
-			                    } else {
-				                    if (resource instanceof IFolder) {
-					                    folders.add((IFolder) resource);
-				                    }
-			                    }
-			                    return false;
-		                    }
+	private static final IDeveloperStudioLog log = Logger.getLog(Activator.PLUGIN_ID);
 
-	                    });
-                    }
-                } catch (CoreException e) {
-	                throw e;
-                }
-                for (IFolder folder : folders) {
-	                map.put(folder, project);
-                }
+	private static final String SERVICES_XML_FILE_NAME = "services.xml";
+	private static final String META_INF_FOLDER_NAME = "META-INF";
+	private static final String SERVICE_ELEMENT_NAME = "service";
+	private static final String PARAMETER_ELEMENT_NAME = "parameter";
+	private static final String NAME_ATTRIBUTE = "name";
+	private static final String SERVICE_CLASS_ATTRIBUTE_VALUE = "ServiceClass";
+	private static final String DOT_SEPARATOR = ".";
+
+	/**
+	 * This method provides logic for getting all Axis2 service folders with related project in workspace.
+	 *
+	 * @return a map contains all service folders with corresponding project
+	 * @throws org.wso2.developerstudio.eclipse.utils.exception.Axis2ServiceUtilsException
+	 */
+	public static Map<IFolder, IProject> getServiceFolders() throws Axis2ServiceUtilsException {
+		HashMap<IFolder, IProject> map = new HashMap<IFolder, IProject>();
+		for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+			if (WebUtils.isDynamicWebProject(project)) {
+				final IFolder servicesFolder;
+				try {
+					servicesFolder = WebUtils.getAxis2WebContainerWEB_INFServicesFolderPath(project);
+				} catch (Exception e) {
+					//some error occurred meaning that this isn't a project type we are looking for, so continue
+					continue;
+				}
+				final List<IFolder> folders = new ArrayList<IFolder>();
+				try {
+					if (servicesFolder.exists()) {
+						servicesFolder.accept(new IResourceVisitor() {
+							public boolean visit(IResource resource) throws CoreException {
+								if (resource.getLocation().toOSString()
+										.equals(servicesFolder.getLocation().toOSString())) {
+									return true;
+								} else {
+									if (resource instanceof IFolder) {
+										folders.add((IFolder) resource);
+									}
+								}
+								return false;
+							}
+
+						});
+					}
+				} catch (CoreException e) {
+					throw new Axis2ServiceUtilsException("Error in getting service folders for Axis2 projects", e);
+				}
+				for (IFolder folder : folders) {
+					map.put(folder, project);
+				}
 			}
 		}
 		return map;
 	}
-	public static String getServiceNameFromFolder(String folderPath) throws Exception{
-		String serviceName=null;
-		String path=FileUtils.addNodesToPath(folderPath, new String[]{"META-INF","services.xml"});
+
+	/**
+	 * This method provides logic for getting Axis2 service name from given resources folder
+	 *
+	 * @param folderPath resources folder path
+	 * @return Axis2 service name
+	 * @throws org.wso2.developerstudio.eclipse.utils.exception.Axis2ServiceUtilsException
+	 */
+	public static String getServiceNameFromFolder(String folderPath) throws Axis2ServiceUtilsException {
+		String path = FileUtils.addNodesToPath(folderPath, new String[]{META_INF_FOLDER_NAME, SERVICES_XML_FILE_NAME});
+		DocumentBuilder documentBuilder = getDocumentBuilder();
+		Document document;
+
 		try {
-			DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document document = parser.parse(new FileInputStream(new File(path)));
-		    serviceName = getServiceNameFromDoc(document);
-		} catch (Exception e) {
-			throw e;
+			document = documentBuilder.parse(new FileInputStream(new File(path)));
+		} catch (SAXException e) {
+			throw new Axis2ServiceUtilsException("Error in parsing services.xml File : " + path + ", to a DOM Document", e);
+		} catch (FileNotFoundException e) {
+			throw new Axis2ServiceUtilsException("Given services XML file not found to parse as a DOM Document, File path : " + path, e);
+		} catch (IOException e) {
+			throw new Axis2ServiceUtilsException("IO exception occurred while parsing given XML file as a DOM Document, File path : "
+					+ path, e);
 		}
-		return serviceName;
+
+		return getServiceNameFromDocument(document);
 	}
-	
-	public static String getServiceNameFromFile(String filePath) throws Exception{
-		String serviceName=null;
+
+	/**
+	 * This method provides logic for getting Axis2 service name from given zip file path
+	 *
+	 * @param filePath zip file path
+	 * @return Axis2 service name
+	 * @throws org.wso2.developerstudio.eclipse.utils.exception.Axis2ServiceUtilsException
+	 */
+	public static String getServiceNameFromFile(String filePath) throws Axis2ServiceUtilsException {
+		ZipFile resourcesZipFile;
+		InputStream inputStreamOfZipFile;
+		DocumentBuilder documentBuilder = getDocumentBuilder();
+		Document document;
+
 		try {
-    		ZipFile zf = new ZipFile(filePath);
-    		InputStream in = zf.getInputStream(new ZipEntry("META-INF/services.xml"));
-			DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document document = parser.parse(in);
-		    serviceName = getServiceNameFromDoc(document);
-		    in.close();
-		    zf.close();
-		} catch (Exception e) {
-			throw e;
+			resourcesZipFile = new ZipFile(filePath);
+			inputStreamOfZipFile = resourcesZipFile.getInputStream(new ZipEntry
+					(META_INF_FOLDER_NAME + File.separator + SERVICES_XML_FILE_NAME));
+		} catch (IOException e) {
+			throw new Axis2ServiceUtilsException("Unable to create an input stream for given zip file, ZIP file path : " + filePath, e);
 		}
-		return serviceName;
-	}
-	public static String getServiceClassNameFromFolder(String folderPath) throws Exception{
-		String serviceName=null;
-		String path=FileUtils.addNodesToPath(folderPath, new String[]{"META-INF","services.xml"});
+
 		try {
-			DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document document = parser.parse(new FileInputStream(new File(path)));
-		    serviceName = getServiceClassNameFromDoc(document);
-		} catch (Exception e) {
-			throw e;
+			document = documentBuilder.parse(inputStreamOfZipFile);
+		} catch (SAXException e) {
+			throw new Axis2ServiceUtilsException("Error in parsing services.xml File : " + filePath + ", to a DOM Document", e);
+		} catch (IOException e) {
+			throw new Axis2ServiceUtilsException("IO exception occurred while parsing given XML file as a DOM Document, File path : "
+					+ filePath, e);
+		} finally {
+			try {
+				inputStreamOfZipFile.close();
+				resourcesZipFile.close();
+			} catch (IOException e) {
+				log.error("Error in closing zip file and input stream, ZIP file : " + filePath, e);
+			}
 		}
-		return serviceName;
+
+		return getServiceNameFromDocument(document);
 	}
-	
-	public static String getServiceClassNameFromFile(String filePath) throws Exception{
-		String serviceName=null;
+
+	/**
+	 * This method provides logic for getting Axis2 service class name from given resources folder
+	 *
+	 * @param folderPath resources folder path
+	 * @return Axis2 service class name
+	 * @throws org.wso2.developerstudio.eclipse.utils.exception.Axis2ServiceUtilsException
+	 */
+	public static String getServiceClassNameFromFolder(String folderPath) throws Axis2ServiceUtilsException {
+		String path = FileUtils.addNodesToPath(folderPath, new String[]{META_INF_FOLDER_NAME, SERVICES_XML_FILE_NAME});
+		DocumentBuilder documentBuilder = getDocumentBuilder();
+		Document document;
+
 		try {
-    		ZipFile zf = new ZipFile(filePath);
-    		InputStream in = zf.getInputStream(new ZipEntry("META-INF/services.xml"));
-			serviceName = getServiceClassNameFromServicesXMLStream(in);
-			in.close();
-			zf.close();
-		} catch (Exception e) {
-			throw e;
+			document = documentBuilder.parse(new FileInputStream(new File(path)));
+		} catch (SAXException e) {
+			throw new Axis2ServiceUtilsException("Error in parsing services.xml File : " + path + ", to a DOM Document", e);
+		} catch (FileNotFoundException e) {
+			throw new Axis2ServiceUtilsException("Given services XML file not found to parse as a DOM Document, File path : " + path, e);
+		} catch (IOException e) {
+			throw new Axis2ServiceUtilsException("IO exception occurred while parsing given XML file as a DOM Document, File path : "
+					+ path, e);
 		}
-		return serviceName;
+
+		return getServiceClassNameFromDocument(document);
 	}
-	
-	public static String getServiceClassNameFromServicesXML(File servicesXml) throws FileNotFoundException, ParserConfigurationException, SAXException, IOException{
+
+	/**
+	 * This method provides logic for getting Axis2 service class name from given zip file path
+	 *
+	 * @param filePath zip file path
+	 * @return Axis2 service class name
+	 * @throws org.wso2.developerstudio.eclipse.utils.exception.Axis2ServiceUtilsException
+	 */
+	public static String getServiceClassNameFromFile(String filePath) throws Axis2ServiceUtilsException {
+		ZipFile resourcesZipFile;
+		InputStream inputStreamOfZipFile;
+		DocumentBuilder documentBuilder = getDocumentBuilder();
+		Document document;
+
+		try {
+			resourcesZipFile = new ZipFile(filePath);
+			inputStreamOfZipFile = resourcesZipFile.getInputStream(new ZipEntry
+					(META_INF_FOLDER_NAME + File.separator + SERVICES_XML_FILE_NAME));
+		} catch (IOException e) {
+			throw new Axis2ServiceUtilsException("Unable to create an input stream for given zip file, ZIP file path : " + filePath, e);
+		}
+
+		try {
+			document = documentBuilder.parse(inputStreamOfZipFile);
+		} catch (SAXException e) {
+			throw new Axis2ServiceUtilsException("Error in parsing services.xml File : " + filePath + ", to a DOM Document", e);
+		} catch (IOException e) {
+			throw new Axis2ServiceUtilsException("IO exception occurred while parsing given XML file as a DOM Document, File path : "
+					+ filePath, e);
+		} finally {
+			try {
+				inputStreamOfZipFile.close();
+				resourcesZipFile.close();
+			} catch (IOException e) {
+				log.error("Error in closing zip file and input stream, ZIP file : " + filePath, e);
+			}
+		}
+
+		return getServiceClassNameFromDocument(document);
+	}
+
+	/**
+	 * This method provides logic for getting Axis2 service class name from given services xml file
+	 *
+	 * @param servicesXml xml file
+	 * @return Axis2 service class name
+	 * @throws javax.xml.parsers.ParserConfigurationException
+	 * @throws org.xml.sax.SAXException
+	 * @throws java.io.IOException
+	 */
+	public static String getServiceClassNameFromServicesXML(File servicesXml)
+			throws ParserConfigurationException, SAXException, IOException {
 		return getServiceClassNameFromServicesXMLStream(new FileInputStream(servicesXml));
 	}
-			
-	public static String getServiceClassNameFromServicesXMLStream(
-			InputStream in) throws ParserConfigurationException, SAXException,
-			IOException {
-		String serviceName;
+
+	/**
+	 * This method provides logic for getting Axis2 service class name from given input stream
+	 *
+	 * @param in input stream of services.xml file
+	 * @return Axis2 service class name
+	 * @throws javax.xml.parsers.ParserConfigurationException
+	 * @throws org.xml.sax.SAXException
+	 * @throws java.io.IOException
+	 */
+	private static String getServiceClassNameFromServicesXMLStream(InputStream in)
+			throws ParserConfigurationException, SAXException, IOException {
 		DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 		Document document = parser.parse(in);
-		serviceName = getServiceClassNameFromDoc(document);
+		return getServiceClassNameFromDocument(document);
+	}
+
+	/**
+	 * This method provides logic for getting Axis2 service name from given xml Document.
+	 * If there are more than one service elements, the service name of first service element will be returned.
+	 *
+	 * @param document org.w3c.dom.Document element
+	 * @return service name
+	 */
+	private static String getServiceNameFromDocument(Document document) {
+		String serviceName;
+		//Getting the first service element of given xml Document, So it will return the first service name of the xml Document
+		Node service = document.getElementsByTagName(SERVICE_ELEMENT_NAME).item(0);
+		Node nameNode = service.getAttributes().getNamedItem(NAME_ATTRIBUTE);
+		serviceName = nameNode.getTextContent();
+
+		//If the service name is null or empty, getting the service class name as the service name
+		if (serviceName == null || serviceName.isEmpty()) {
+			String serviceClassName = getServiceClassNameFromDocument(document);
+			String[] split = serviceClassName.split(DOT_SEPARATOR);
+			serviceName = split[split.length - 1];
+		}
 		return serviceName;
 	}
-	private static String getServiceNameFromDoc(Document document) {
-	    String serviceName;
-	    serviceName = document.getDocumentElement().getAttribute("name");
-	    if (serviceName==null || serviceName.equals("")){
-	    	NodeList nodes = document.getDocumentElement().getElementsByTagName("service");
-	    	if(nodes.getLength() != 0){
-	    		serviceName=nodes.item(0).getAttributes().getNamedItem("name").getNodeValue();
-	    	}
-	    }
-	    if(serviceName==null || serviceName.equals("")){
-	    	String serviceClassName = getServiceClassNameFromDoc(document);
-	    	String[] split = serviceClassName.split(".");
-	    	serviceName=split[split.length-1];
-	    }
-	    return serviceName;
-    }
-	
-	private static String getServiceClassNameFromDoc(Document document) {
-	    String serviceClassName = null;
-		NodeList serviceChildNodes = null;
-	    if (document.getDocumentElement().getNodeName().equals("service")){
-	    	serviceChildNodes=document.getDocumentElement().getChildNodes();
-	    }else{
-	    	NodeList childNodes = document.getDocumentElement().getChildNodes();
-	    	for(int i = 0; i< childNodes.getLength(); i++){
-	    		if(childNodes.item(i).getNodeName().equals("service")){
-	    			serviceChildNodes = childNodes.item(i).getChildNodes();
-	    			break;
-	    		}
-	    	}
-	    }
-    	if (serviceChildNodes!=null){
-        	for(int j =0 ; j < serviceChildNodes.getLength(); j++){
-    			Node node = serviceChildNodes.item(j);
-				if(node.getAttributes() != null){
-					if (node.getNodeName().equalsIgnoreCase("parameter")){
-        				Node attribute = node.getAttributes().getNamedItem("name");
-    					if(attribute!=null && attribute.getNodeValue().equals("ServiceClass")){
-        					serviceClassName = node.getTextContent();
-        					break;
-        				}
-					}
-    			}
-    			
-    		}
-    	}
-	    return serviceClassName;
-    }
+
+	/**
+	 * This method provides logic for getting Axis2 service class name from given xml Document.
+	 * If there are more than one service elements, the service class name of first service element will be returned.
+	 *
+	 * @param document org.w3c.dom.Document element
+	 * @return service class name
+	 */
+	private static String getServiceClassNameFromDocument(Document document) {
+		String serviceClassName = null;
+		//Getting the first service element from xml Document
+		Node service = document.getElementsByTagName(SERVICE_ELEMENT_NAME).item(0);
+		NodeList serviceChildNodes = service.getChildNodes();
+
+		//Loop through all child nodes to get 'ServiceClass' parameter value
+		for (int j = 0; j < serviceChildNodes.getLength(); j++) {
+			Node node = serviceChildNodes.item(j);
+			if (PARAMETER_ELEMENT_NAME.equals(node.getNodeName())) {
+				NamedNodeMap attr = node.getAttributes();
+				Node nodeAttr = attr.getNamedItem(NAME_ATTRIBUTE);
+				if (nodeAttr.getNodeValue().equals(SERVICE_CLASS_ATTRIBUTE_VALUE)) {
+					serviceClassName = node.getTextContent();
+				}
+			}
+		}
+		return serviceClassName;
+	}
+
+	/**
+	 * Method that returns javax.xml.parsers.DocumentBuilder
+	 *
+	 * @return DocumentBuilder
+	 * @throws org.wso2.developerstudio.eclipse.utils.exception.Axis2ServiceUtilsException
+	 */
+	private static DocumentBuilder getDocumentBuilder() throws Axis2ServiceUtilsException {
+		try {
+			return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new Axis2ServiceUtilsException("Unable to create a DOM Document builder using javax XML builder factory", e);
+		}
+	}
 
 }

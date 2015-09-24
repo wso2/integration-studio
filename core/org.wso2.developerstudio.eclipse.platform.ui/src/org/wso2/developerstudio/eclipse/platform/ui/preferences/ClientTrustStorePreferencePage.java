2dad92b0e6ee6fc2dd6304a2dfedccfe9af7abf7
Jasintha <jasinthad@gmail.com>
2015-09-24 12:00:04 +0530

    JVM need to restart to apply trustore chanages

--- core/org.wso2.developerstudio.eclipse.platform.ui/src/org/wso2/developerstudio/eclipse/platform/ui/preferences/ClientTrustStorePreferencePage.java
+++ core/org.wso2.developerstudio.eclipse.platform.ui/src/org/wso2/developerstudio/eclipse/platform/ui/preferences/ClientTrustStorePreferencePage.java
@@ -1,6 +1,19 @@
+/* Copyright (c) WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wso2.developerstudio.eclipse.platform.ui.preferences;
 
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.ComboFieldEditor;
@@ -15,8 +28,7 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.PlatformUI;
 import org.wso2.developerstudio.eclipse.platform.ui.Activator;
 
-public class ClientTrustStorePreferencePage extends FieldEditorPreferencePage
-		implements IWorkbenchPreferencePage {
+public class ClientTrustStorePreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 	public static final String TRUST_STORE_PASSWORD = "TRUST_STORE_PASSWORD";
 	public static final String SHOW_PLAIN_PASSWORD = "SHOW_PLAIN_PASSWORD";
 	public static final String TRUST_STORE_TYPE = "TRUST_STORE_TYPE";
@@ -37,57 +49,56 @@ public class ClientTrustStorePreferencePage extends FieldEditorPreferencePage
 
 	@Override
 	protected void createFieldEditors() {
-		addField(new FileFieldEditor(TRUST_STORE_LOCATION,
-				"&Trust Store Location:", getFieldEditorParent()));
-		String[][] types = { { "JKS", "JKS" },{"JCEKS","JCEKS"},{"PKCS12","PKCS12"} };
+		addField(new FileFieldEditor(TRUST_STORE_LOCATION, "&Trust Store Location:", getFieldEditorParent()));
+		String[][] types = { { "JKS", "JKS" }, { "JCEKS", "JCEKS" }, { "PKCS12", "PKCS12" } };
 		ComboFieldEditor editor = new ComboFieldEditor(TRUST_STORE_TYPE, "Trust &Store Type", types,
 				getFieldEditorParent());
-		 
+
 		addField(editor);
-		stringField1 = new StringFieldEditor(TRUST_STORE_PASSWORD,
-				"Trust Store &Password", getFieldEditorParent());
+		stringField1 = new StringFieldEditor(TRUST_STORE_PASSWORD, "Trust Store &Password", getFieldEditorParent());
 		addField(stringField1);
-		BooleanFieldEditor booleanFieldEditor = new BooleanFieldEditor(
-				SHOW_PLAIN_PASSWORD, "S&how Password in Plain Text",
-				getFieldEditorParent());
+		BooleanFieldEditor booleanFieldEditor = new BooleanFieldEditor(SHOW_PLAIN_PASSWORD,
+				"S&how Password in Plain Text", getFieldEditorParent());
 		addField(booleanFieldEditor);
 		if (!preferenceStore.getBoolean(SHOW_PLAIN_PASSWORD)) {
-			stringField1.getTextControl(getFieldEditorParent())
-					.setEchoChar('*');
+			stringField1.getTextControl(getFieldEditorParent()).setEchoChar('*');
 		}
-		preferenceStore
-				.addPropertyChangeListener(new IPropertyChangeListener() {
+		preferenceStore.addPropertyChangeListener(new IPropertyChangeListener() {
 
-					@Override
-					public void propertyChange(
-							org.eclipse.jface.util.PropertyChangeEvent arg0) {
+			@Override
+			public void propertyChange(org.eclipse.jface.util.PropertyChangeEvent arg0) {
 
-						if (arg0.getProperty() == SHOW_PLAIN_PASSWORD) {
-							boolean boolean1 = preferenceStore
-									.getBoolean(SHOW_PLAIN_PASSWORD);
-							if (boolean1) {
-								stringField1.getTextControl(
-										getFieldEditorParent()).setEchoChar(
-										'\0');
-							} else {
-								stringField1.getTextControl(
-										getFieldEditorParent())
-										.setEchoChar('*');
-							}
-							
-						}
-
-						if (arg0.getProperty() == TRUST_STORE_LOCATION) {
-							String string = preferenceStore
-									.getString(TRUST_STORE_LOCATION);
-							if (!string.equals("") && !string.endsWith(".jks")) {
-								MessageDialog.openError(Display.getCurrent().getActiveShell(),
-										"Error Dialog", "You cannot set non JKS trust stores from Developer Studio");
-							}
-						}
-						
+				if (SHOW_PLAIN_PASSWORD.equals(arg0.getProperty())) {
+					boolean boolean1 = preferenceStore.getBoolean(SHOW_PLAIN_PASSWORD);
+					if (boolean1) {
+						stringField1.getTextControl(getFieldEditorParent()).setEchoChar('\0');
+					} else {
+						stringField1.getTextControl(getFieldEditorParent()).setEchoChar('*');
 					}
-				});		
+				}
+
+				if (TRUST_STORE_LOCATION.equals(arg0.getProperty())) {
+					String string = preferenceStore.getString(TRUST_STORE_LOCATION);
+					if (!string.equals("") && !string.endsWith(".jks")) {
+						MessageDialog.openError(Display.getCurrent().getActiveShell(), "Developer Studio Error Dialog",
+								"You cannot set non JKS trust stores from Developer Studio");
+					}
+				}
+								
+			}
+		});
+		
+		
+	}
+	
+	@Override
+	public boolean performOk() {
+		boolean performOk = super.performOk();
+		String message = "You must restart eclipse to apply these chnages, Restart Now ?";
+		if(MessageDialog.openQuestion(Display.getCurrent().getActiveShell(),"Developer Studio Info", message)){
+			PlatformUI.getWorkbench().restart();
+		}
+		return performOk;
 	}
 
 }

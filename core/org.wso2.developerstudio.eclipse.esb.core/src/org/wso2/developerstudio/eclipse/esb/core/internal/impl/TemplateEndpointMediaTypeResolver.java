294975f1b57ac09a84a5ac74450cf351d33dfae7
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2015-10-07 11:47:47 +0530

    Revert "Fixing issue with media type resolvers"
    This reverts commit e0922712bf9ee919858eb3457c01921f358a5a07.

--- core/org.wso2.developerstudio.eclipse.esb.core/src/org/wso2/developerstudio/eclipse/esb/core/internal/impl/TemplateEndpointMediaTypeResolver.java
+++ core/org.wso2.developerstudio.eclipse.esb.core/src/org/wso2/developerstudio/eclipse/esb/core/internal/impl/TemplateEndpointMediaTypeResolver.java
@@ -36,7 +36,7 @@ public class TemplateEndpointMediaTypeResolver extends AbstractXmlDocMediaTypeRe
                 Iterator endpointElementItrtr = root.getChildrenWithLocalName("endpoint");
                 isEndpointTemplate = endpointElementItrtr.hasNext(); 
             }
-        } catch (Exception e) {
+        } catch (XMLStreamException | FactoryConfigurationError e) {
             isEndpointTemplate = false;
         }
         return isEndpointTemplate;

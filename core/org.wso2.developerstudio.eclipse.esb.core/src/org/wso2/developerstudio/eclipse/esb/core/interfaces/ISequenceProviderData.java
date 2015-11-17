392da36dc53749e69f8e2b14253c66f234dea532
Rajeeva <rajeeva@wso2.com>
2015-11-17 16:49:17 +0530

    remove core

--- core/org.wso2.developerstudio.eclipse.esb.core/src/org/wso2/developerstudio/eclipse/esb/core/interfaces/ISequenceProviderData.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.developerstudio.eclipse.esb.core.interfaces;
-
-import org.wso2.developerstudio.eclipse.platform.core.interfaces.IDeveloperStudioProviderData;
-
-public interface ISequenceProviderData extends IDeveloperStudioProviderData {
-	public ISequenceProvider getProvider();
-}

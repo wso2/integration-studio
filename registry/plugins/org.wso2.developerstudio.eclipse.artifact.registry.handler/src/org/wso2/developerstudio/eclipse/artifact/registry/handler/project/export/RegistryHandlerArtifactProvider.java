1f0e64fe808a447861c13d82d2dcdbf3770747da
Rajeeva <rajeeva@wso2.com>
2015-12-07 19:01:02 +0530

    extending CappArtifactsListProvider for registry filter and handler project

---
+++ registry/plugins/org.wso2.developerstudio.eclipse.artifact.registry.handler/src/org/wso2/developerstudio/eclipse/artifact/registry/handler/project/export/RegistryHandlerArtifactProvider.java
@@ -0,0 +1,26 @@
+package org.wso2.developerstudio.eclipse.artifact.registry.handler.project.export;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.wso2.developerstudio.eclipse.artifact.registry.handler.Activator;
+import org.wso2.developerstudio.eclipse.distribution.project.export.CappArtifactsListProvider;
+import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
+import org.wso2.developerstudio.eclipse.logging.core.Logger;
+
+public class RegistryHandlerArtifactProvider extends CappArtifactsListProvider {
+	private static IDeveloperStudioLog log = Logger.getLog(Activator.PLUGIN_ID);
+
+	@Override
+	public List<ListData> getArtifactsListForCappExport(IProject project) {
+
+		List<ListData> list = new ArrayList<ListData>();
+		try {
+			list = super.getArtifactsListForCappExport(project);
+		} catch (Exception e) {
+			log.error("Error getting artifacts list from project ", e);
+		}
+		return list;
+	}
+}

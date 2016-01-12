0e0ff68bd67ebb4cc1b862dd376c432ba513f1c0
susinda <susinda@wso2.com>
2016-01-12 18:31:58 +0530

    delete implemented, inputBox loading implemented

--- datamapper-tool/org.wso2.developerstudio.datamapper.js.editor/DatamapperJsEditor/js/designEditor.js
+++ datamapper-tool/org.wso2.developerstudio.datamapper.js.editor/DatamapperJsEditor/js/designEditor.js
@@ -1,4 +1,8 @@
 
+var highlightCell=null;
+var childHeight = 30;
+var leafHeight = 30;
+
 
 function initDraggable() {
     $(".draggableIcon").draggable({
@@ -8,6 +12,23 @@ function initDraggable() {
         stop: handleDragStopEvent
     });
 }
+
+paper.on('cell:pointerdown', function(cellView) {
+    selected = cellView.model;
+    //console.log("selected = " + selected);
+    if(cellView.model instanceof  joint.dia.Link) return;
+    if(highlightCell) {
+    	highlightCell.unhighlight();
+    }
+    highlightCell=cellView;
+    cellView.highlight();
+});
+
+paper.on('blank:pointerclick', function(cellView) {
+    selected = null;
+    if(highlightCell) highlightCell.unhighlight();
+    highlightCell=null;
+});
 
 function initDropable() {
     $("#dmEditorContainer").droppable({
@@ -23,7 +44,11 @@ function initJointJSGraph() {
         el: $('#dmEditorContainer'),
         width: 1200,
         model: graph,
-        gridSize: 1
+        gridSize: 1,
+        interactive: function(cellView, methodName) {
+            if (cellView.model.get('isInteractive') === false) return false;
+            return true;
+        }
     });
     
     paper.on('cell:pointerdown', function(cellView) {
@@ -35,10 +60,9 @@ function initJointJSGraph() {
     	id:'inputBox',
 	    position: { x: 50, y: 50 },
 	    size: { width: 150, height: 400 },
-	    outPorts: ['out'],
 	    attrs: {
-	        '.label': { text: 'Input Type', 'ref-x': .4, 'ref-y': .2 },
-	        rect: { fill: 'gray' },
+	        '.label': { text: 'Input Type', 'text-anchor':'start', 'ref-x': .1},
+	        rect: { fill: '#d3d3d3' },
 	        '.outPorts circle': { fill: '#E74C3C' }
 	    }
 	});
@@ -49,7 +73,7 @@ function initJointJSGraph() {
 	    size: { width: 150, height: 400 },
 	    inPorts: ['in'],
 	    attrs: {
-	        '.label': { text: 'Output Type', 'ref-x': .4, 'ref-y': .2 },
+	        '.label': { text: 'Output Type', 'text-anchor':'start', 'ref-x': .1}, 
 	        rect: { fill: 'gray' },
 	        '.inPorts circle': { fill: '#E74C3C' }
 	    }
@@ -58,9 +82,120 @@ function initJointJSGraph() {
 	graph.addCell(mInput);
 	graph.addCell(mOutput);
 	
+	var child1 = addOutputChild(mInput, 'Child001');
+    var child2 = addOutputChild(mInput, 'Child002');
+ 	var child21 = addOutputChild(child2, 'Child0021');
+ 	var child211 = addOutputChild(child21, 'Child00211');
+ 	
+    addOutputLeaf(child21, 'leaf2001');
+    addOutputLeaf(child21, 'leaf2002');
+    resizeHeightAtTheEnd(child21);
+    resizeHeightAtTheEnd(child2);
+	//addOutputLeaf(child21, 'leaf2003');
+	//var child22 = addOutputChild(child2, 'Child0022');
+	
+	var child3 = addOutputChild(mInput, 'Child003');
+	//var child31 = addOutputChild(child3, 'Child0031');
+
+ 	resizeHeightAtTheEnd(mInput);
+	
+	
     //graph.fromJSON(JSON.parse('{"cells":[{"type":"basic.Rect","position":{"x":100,"y":30},"size":{"width":100,"height":30},"angle":0,"id":"4713e533-6d0f-48e3-8098-2fee3364d023","z":1,"attrs":{"rect":{"fill":"green"},"text":{"fill":"red","text":"my box1"}}},{"type":"basic.Rect","position":{"x":238,"y":107},"size":{"width":100,"height":30},"angle":0,"id":"426e4cea-bb7e-46db-b6a2-75331764385b","embeds":"","z":2,"attrs":{"rect":{"fill":"green"},"text":{"fill":"red","text":"my box1"}}},{"type":"link","source":{"id":"4713e533-6d0f-48e3-8098-2fee3364d023"},"target":{"id":"426e4cea-bb7e-46db-b6a2-75331764385b"},"id":"e32c2313-2f15-477d-804d-fd1c327b5944","z":3,"attrs":{}}]}'));
 
 }
+
+function addOutputChild(parent, childName) {
+	var parentPosition = parent.get('position');
+	
+	var embededCells = parent.getEmbeddedCells();
+	var embededCellsLength = parent.getEmbeddedCells().length;
+	var totalHeight = 0;
+	for (var i = 0; i < embededCellsLength; i++) {
+		totalHeight += embededCells[i].get('size').height;
+	}
+	
+	var newColor = getColorFromParent(parent);
+	var childX = parentPosition.x + 20;
+	var childY = parentPosition.y + childHeight + totalHeight;
+	var child = new joint.shapes.basic.Rect({
+		isInteractive: false,
+	    position: { x: childX, y: childY },
+	    size: { width: 100, height: leafHeight },
+	    attrs: { rect: { fill: newColor,  stroke: 'none'}, text: { text: childName, 'text-anchor':'start', 'ref-x': .1, 'ref-y': .2} }
+	});
+
+	var parentHeight = 30 + childHeight + totalHeight;
+	alert(totalHeight);
+	parent.set({
+	     position: parent.get('position'),
+	     size: { width: 100, height: parentHeight }
+	});
+	
+	parent.embed(child);
+	graph.addCell(child);
+	return child;
+}
+
+function resizeHeightAtTheEnd(element){
+	var embededCells = element.getEmbeddedCells();
+	var embededCellsLength = element.getEmbeddedCells().length;
+	var totalHeight = 0;
+	for (var i = 0; i < embededCellsLength; i++) {
+		totalHeight += embededCells[i].get('size').height;
+	}
+	var parentHeight = 30 + totalHeight;
+	element.set({
+	     position: element.get('position'),
+	     size: { width: 100, height: parentHeight }
+	});
+}
+
+function getColorFromParent(parent) {
+	var color = parent.attr('rect/fill');
+	var colorCode = color.substring(1,7);
+	var step = '60606';
+	var newColor = parseInt(colorCode, 16) + parseInt(step, 16);
+	newColor = '#' + newColor.toString(16);
+	return newColor;
+}
+
+
+function addOutputLeaf( parent, leafName ) {
+	var leafHeight = 20;
+	var parentPosition = parent.get('position');
+	var newColor = getColorFromParent(parent);
+	
+	var embededCells = parent.getEmbeddedCells();
+	var embededCellsLength = parent.getEmbeddedCells().length;
+	var totalHeight = 0;
+	for (var i = 0; i < embededCellsLength; i++) {
+		totalHeight += embededCells[i].get('size').height;
+	}
+	
+	var childX = parentPosition.x + 20;
+	var childY = parentPosition.y + leafHeight + totalHeight;
+	var outLeaf = new joint.shapes.devs.Model({
+    	isInteractive: false,
+	    size: { width: 80, height: leafHeight },
+	    position: {x:childX, y:childY},
+	    outPorts: [''],
+	    attrs: {
+	        '.label': { text: leafName, 'text-anchor':'start', 'ref-x': .1, 'ref-y': .2 },
+	        rect: { fill: newColor,  stroke: 'none' },
+	        '.outPorts circle': { fill: '#E74C3C' }
+	    }
+	});
+	
+	 parent.set({
+         position: parent.get('position'),
+         size: { width: 100, height: 30 + leafHeight + totalHeight }
+     });
+
+	parent.embed(outLeaf);
+    graph.addCell(outLeaf);
+}
+
+
 
 
 function handleDragStopEvent( event, ui ) {

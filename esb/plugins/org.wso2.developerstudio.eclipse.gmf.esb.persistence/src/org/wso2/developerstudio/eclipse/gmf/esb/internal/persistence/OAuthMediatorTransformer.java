3a39545973e919414f59f73ce99c4fa8f48c40b0
Nuwan Chamara Pallewela <nwnpallewela@gmail.com>
2016-01-15 21:18:00 +0530

    Refactoring and Removing operations from BAMMediatorTransformer and OAuthMediatorTransformer

--- esb/plugins/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/OAuthMediatorTransformer.java
+++ esb/plugins/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/OAuthMediatorTransformer.java
@@ -40,36 +40,42 @@ public class OAuthMediatorTransformer extends AbstractEsbNodeTransformer  {
 	}
 	
 	private org.wso2.carbon.identity.oauth.mediator.OAuthMediator createOAuthMediator(EsbNode subject){
-		// Check subject.
-		Assert.isTrue(subject instanceof OAuthMediator, "Invalid subject."); //$NON-NLS-1$
-		OAuthMediator visualOauth = (OAuthMediator) subject;
 
-		// Configure properties of the mediator.
-		org.wso2.carbon.identity.oauth.mediator.OAuthMediator OauthMediator = new org.wso2.carbon.identity.oauth.mediator.OAuthMediator();
-		setCommonProperties(OauthMediator, visualOauth);
-		{
-			if (StringUtils.isNotBlank(visualOauth.getRemoteServiceUrl())) {
-				OauthMediator.setRemoteServiceUrl(visualOauth.getRemoteServiceUrl());
-			} else {
-				throw new IllegalArgumentException(
-						Messages.OAuthMediatorTransformer_Remote_Server_Url_Required_Error_Message);
+		if (subject instanceof OAuthMediator) {
+			OAuthMediator visualOauth = (OAuthMediator) subject;
+
+			// Configure properties of the mediator.
+			org.wso2.carbon.identity.oauth.mediator.OAuthMediator OauthMediator = new org.wso2.carbon.identity.oauth.mediator.OAuthMediator();
+			setCommonProperties(OauthMediator, visualOauth);
+			{
+				if (StringUtils.isNotBlank(visualOauth.getRemoteServiceUrl())) {
+					OauthMediator.setRemoteServiceUrl(visualOauth
+							.getRemoteServiceUrl());
+				} else {
+					throw new IllegalArgumentException(
+							Messages.OAuthMediatorTransformer_Remote_Server_Url_Required_Error_Message);
+				}
+
+				if (StringUtils.isNotBlank(visualOauth.getUsername())) {
+					OauthMediator.setUsername(visualOauth.getUsername());
+				} else {
+					throw new IllegalArgumentException(
+							Messages.OAuthMediatorTransformer_Username_Required_Error_Message);
+				}
+
+				if (StringUtils.isNotBlank(visualOauth.getPassword())) {
+					OauthMediator.setPassword(visualOauth.getPassword());
+				} else {
+					throw new IllegalArgumentException(
+							Messages.OAuthMediatorTransformer_Password_Required_Error_Message);
+				}
 			}
-			
-			if (StringUtils.isNotBlank(visualOauth.getUsername())) {
-				OauthMediator.setUsername(visualOauth.getUsername());
-			} else {
-				throw new IllegalArgumentException(
-						Messages.OAuthMediatorTransformer_Username_Required_Error_Message);
-			}
-			
-			if (StringUtils.isNotBlank(visualOauth.getPassword())) {
-				OauthMediator.setPassword(visualOauth.getPassword());
-			} else {
-				throw new IllegalArgumentException(
-						Messages.OAuthMediatorTransformer_Password_Required_Error_Message);
-			}
+			return OauthMediator;
+		} else {
+			throw new IllegalArgumentException(
+					Messages.OAuthMediatorTransformer_InvalidSubjectErrorMessage);
 		}
-		return OauthMediator;
+
 	}
 
 }

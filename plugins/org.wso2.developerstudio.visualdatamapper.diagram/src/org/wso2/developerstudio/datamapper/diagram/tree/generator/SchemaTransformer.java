dd57708a49e1987c78d0a38f456b602f54226265
awanthika <wnthksnrth@gmail.com>
2016-03-15 10:17:04 +0530

    adding annotations jar and model to json generator logic

--- plugins/org.wso2.developerstudio.visualdatamapper.diagram/src/org/wso2/developerstudio/datamapper/diagram/tree/generator/SchemaTransformer.java
+++ plugins/org.wso2.developerstudio.visualdatamapper.diagram/src/org/wso2/developerstudio/datamapper/diagram/tree/generator/SchemaTransformer.java
@@ -25,8 +25,16 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
 import org.apache.commons.io.FileUtils;
 import org.wso2.developerstudio.datamapper.DataMapperFactory;
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.EMap;
+import org.wso2.developerstudio.datamapper.Element;
 import org.wso2.developerstudio.datamapper.TreeNode;
 import org.wso2.developerstudio.datamapper.diagram.Activator;
 import org.wso2.developerstudio.datamapper.diagram.tree.model.Tree;
@@ -34,13 +42,26 @@ import org.wso2.developerstudio.datamapper.impl.TreeNodeImpl;
 import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
 import org.wso2.developerstudio.eclipse.logging.core.Logger;
 
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import de.odysseus.staxon.json.JsonXMLOutputFactory;
 
 public class SchemaTransformer implements ISchemaTransformer {
 
+	private static final String JSON_SCHEMA_TITLE = "title";
+	private static final String JSON_SCHEMA_ITEMS = "items";
+	private static final String JSON_SCHEMA_STRING = "string";
+	private static final String JSON_SCHEMA_ARRAY = "array";
+	private static final String JSON_SCHEMA_ID = "id";
+	private static final String JSON_SCHEMA_NAME = "name";
+	private static final String JSON_SCHEMA_PROPERTIES = "properties";
+	private static final String JSON_SCHEMA_TYPE = "type";
+	private static final String JSON_SCHEMA_OBJECT = "object";
 	private static IDeveloperStudioLog log = Logger.getLog(Activator.PLUGIN_ID);
 	private static String ERROR_TEXT = "File cannot be found ";
 	private static String ERROR_WRITING_SCHEMA_FILE = "Error writing to schema file";
@@ -53,7 +74,6 @@ public class SchemaTransformer implements ISchemaTransformer {
 	private static final String ITEMS_KEY = "items";
 	private static final String OBJECT_ELEMENT_TYPE = "object";
 	private static final String ARRAY_ELEMENT_TYPE = "array";
-	private String name;
 	
 	
 	@SuppressWarnings("unchecked")
@@ -62,19 +82,19 @@ public class SchemaTransformer implements ISchemaTransformer {
 		
 		InputStream schema = new ByteArrayInputStream(content.getBytes());
 		ObjectMapper objectMapper = new ObjectMapper();
-	            jsonSchemaMap = objectMapper.readValue(schema, Map.class);
-	            //Sets the name of the root
-	            inputRootTreeNode.setName(getName(jsonSchemaMap));
-	            //Sets the level for the root
-	            inputRootTreeNode.setLevel(1);
-	            //Sets the type of the root
-	            if (inputRootTreeNode.getProperties().containsKey(TYPE_KEY)){
-		        	//treeNode.getProperties().get("type")
-		        } else  {
-		        	inputRootTreeNode.getProperties().put(TYPE_KEY, getSchemaType(jsonSchemaMap));
-		        }
-	            int count =1;
-	            inputRootTreeNode = setProperties(jsonSchemaMap,inputRootTreeNode,count);
+        jsonSchemaMap = objectMapper.readValue(schema, Map.class);
+        //Sets the name of the root
+        inputRootTreeNode.setName(getName(jsonSchemaMap));
+        //Sets the level for the root
+        inputRootTreeNode.setLevel(1);
+        //Sets the type of the root
+        if (inputRootTreeNode.getProperties().containsKey(TYPE_KEY)){
+        	//treeNode.getProperties().get("type")
+        } else  {
+        	inputRootTreeNode.getProperties().put(TYPE_KEY, getSchemaType(jsonSchemaMap));
+        }
+        int count =1;
+        inputRootTreeNode = setProperties(jsonSchemaMap,inputRootTreeNode,count);
 	            
 		return inputRootTreeNode;
 		 
@@ -183,7 +203,8 @@ public class SchemaTransformer implements ISchemaTransformer {
 			return inputRootTreeNode;
 		 
 	 }
-	  
+
+
 	@Override
 	public String getSchemaContentFromFile(String path) {
 		File avsc = new File(path);
@@ -198,7 +219,70 @@ public class SchemaTransformer implements ISchemaTransformer {
 
 	@Override
 	public String getSchemaContentFromModel(TreeNodeImpl treeNodeModel) {
+		String name = null;
+		JsonFactory jscksonFactory = new JsonFactory();
+		try {
+			JsonGenerator jGenerator = jscksonFactory.createGenerator(new File("data/output.json"), JsonEncoding.UTF8);
+			if (StringUtils.isNotEmpty(treeNodeModel.getName())) {
+				recursiveTreeGenerator(treeNodeModel, jGenerator);
+			}
+		} catch (IOException | XMLStreamException e) {
+			//log error
+		}
 		return name;
+	}
+
+	private void recursiveTreeGenerator(TreeNodeImpl treeNodeModel,
+			JsonGenerator jGenerator) throws XMLStreamException, IOException {
+		EMap<String, String> propertyMap = treeNodeModel.getProperties();
+		EList<Element> elemList = treeNodeModel.getElement();
+		EList<TreeNode> nodeList = treeNodeModel.getNode();
+		JsonFactory factory = new JsonFactory();
+//		jGenerator.close();
+		
+		if (!propertyMap.get(JSON_SCHEMA_TITLE).isEmpty()
+				&& propertyMap.get(JSON_SCHEMA_TITLE) != null)
+			jGenerator.writeStringField(JSON_SCHEMA_TITLE, propertyMap.get(JSON_SCHEMA_TITLE)); // "name" : "mkyong"
+		jGenerator.writeStringField(JSON_SCHEMA_ID, propertyMap.get(JSON_SCHEMA_ID)); // "name" : "mkyong"
+		String schemaValType = propertyMap.get(JSON_SCHEMA_TYPE);
+		// if type is Object
+		if (schemaValType != null && schemaValType.equals(JSON_SCHEMA_OBJECT)) {
+			for (Element elem : elemList) {
+				jGenerator.writeStringField(JSON_SCHEMA_TYPE,schemaValType);
+				jGenerator.writeFieldName(JSON_SCHEMA_PROPERTIES);
+				jGenerator.writeFieldName(JSON_SCHEMA_NAME);
+				generateElement(jGenerator, elem);
+			}
+		} else if (schemaValType != null
+				&& schemaValType.equals(JSON_SCHEMA_ARRAY)) {
+			jGenerator.writeStringField(JSON_SCHEMA_TYPE,schemaValType);
+			jGenerator.writeFieldName(JSON_SCHEMA_ITEMS);
+			schemaValType = propertyMap.get(JSON_SCHEMA_TYPE);
+			for (TreeNode node : nodeList) {
+				recursiveTreeGenerator((TreeNodeImpl) node, jGenerator);
+			}
+		} else if (schemaValType != null
+				&& schemaValType.equals(JSON_SCHEMA_STRING)) {
+			jGenerator.writeStringField(JSON_SCHEMA_TYPE,schemaValType);
+			jGenerator.writeEndObject();
+		}
+	}
+
+	private void generateElement(JsonGenerator jGenerator,
+			Element elem)
+			throws XMLStreamException {
+		if (elem.getValue() != null && elem.getValue().equals(JSON_SCHEMA_STRING)) {
+			
+			try {
+				jGenerator.writeStringField(JSON_SCHEMA_ID,elem.getName());
+				jGenerator.writeStringField(JSON_SCHEMA_TYPE,elem.getValue());
+				jGenerator.writeEndObject();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				//log
+			}
+			
+		}
 	}
 
 	@Override

54d02f9bee4536955fc01665b8b2b0c09c25839c
awanthika <wnthksnrth@gmail.com>
2016-04-12 09:10:07 +0530

    including the web UI tooling in platform tooling

---
+++ plugins/org.wso2.developerstudio.webeditor/src/org/wso2/developerstudio/webeditor/handler/BrowserCopyActionHandler.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.developerstudio.webeditor.handler;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuCreator;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.swt.events.HelpListener;
+import org.eclipse.swt.widgets.Event;
+
+public class BrowserCopyActionHandler implements IAction {
+
+	@Override
+	public void addPropertyChangeListener(IPropertyChangeListener arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getAccelerator() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getActionDefinitionId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageDescriptor getDisabledImageDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HelpListener getHelpListener() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageDescriptor getHoverImageDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageDescriptor getImageDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IMenuCreator getMenuCreator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getStyle() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getToolTipText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isChecked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isHandled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removePropertyChangeListener(IPropertyChangeListener arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void runWithEvent(Event arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAccelerator(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setActionDefinitionId(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setChecked(boolean arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDescription(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDisabledImageDescriptor(ImageDescriptor arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setEnabled(boolean arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHelpListener(HelpListener arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHoverImageDescriptor(ImageDescriptor arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setId(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setImageDescriptor(ImageDescriptor arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setMenuCreator(IMenuCreator arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setText(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setToolTipText(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

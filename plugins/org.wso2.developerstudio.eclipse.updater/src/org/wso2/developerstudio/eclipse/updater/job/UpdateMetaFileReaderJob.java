cdb81056612de03ed3854e421c97899d96fcd63c
awanthika <wnthksnrth@gmail.com>
2016-05-05 13:42:00 +0530

    code review suggestions and SNAPSHOT zip delete ant run task

--- plugins/org.wso2.developerstudio.eclipse.updater/src/org/wso2/developerstudio/eclipse/updater/job/UpdateMetaFileReaderJob.java
+++ plugins/org.wso2.developerstudio.eclipse.updater/src/org/wso2/developerstudio/eclipse/updater/job/UpdateMetaFileReaderJob.java
@@ -20,6 +20,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URL;
 import java.util.Collection;
 import java.util.HashMap;
@@ -52,6 +53,7 @@ import org.wso2.developerstudio.eclipse.updater.core.UpdateManager;
  */
 public class UpdateMetaFileReaderJob extends Job {
 
+	private static final int SUB_MONITOR_INDEX = 2;
 	private static final int AVAILABLE_VERSION_LESS = -1;
 	private static final int AVAILABLE_VERSION_GREATER = 1;
 	private static final String FEATURE_GROUP_APPEND = ".feature.group";
@@ -86,40 +88,7 @@ public class UpdateMetaFileReaderJob extends Job {
 	@Override
 	protected IStatus run(IProgressMonitor monitor) {
 		try {
-			SubMonitor progress = SubMonitor.convert(monitor, Messages.UpdateCheckerJob_1, 2);
-			Collection<IInstallableUnit> installedWSO2Features = updateManager
-					.getInstalledWSO2Features(progress.newChild(1));
-			// read the meta file
-			if (!downloadMetaFile()) {
-				promptUserError(Messages.UpdateMetaFileReaderJob_3, ERROR_TITLE);
-				return Status.CANCEL_STATUS;
-			}
-			Map<String, String> availaleDevStudioFeatureVerions = new HashMap<String, String>();
-			availaleDevStudioFeatureVerions = readMetaDataFiletoMap();
-			deleteDownloadedTempFile();
-			if (availaleDevStudioFeatureVerions.isEmpty()) {
-				log.error(Messages.UpdateCheckerJob_4);
-				return Status.CANCEL_STATUS;
-			}
-			for (IInstallableUnit iInstallableUnit : installedWSO2Features) {
-				if (availaleDevStudioFeatureVerions.containsKey(iInstallableUnit.getId())) {
-					Version availableVersion = generateVersionFromString(
-							availaleDevStudioFeatureVerions.get(iInstallableUnit.getId()));
-					Version upperLimit = generateUpperLimitforUpdates(iInstallableUnit.getVersion());
-					if (availableVersion != null
-							&& availableVersion.compareTo(iInstallableUnit.getVersion()) == AVAILABLE_VERSION_GREATER
-							&& availableVersion.compareTo(upperLimit) == AVAILABLE_VERSION_LESS) {
-						updateCount = updateCount + 1;
-					} else if (availableVersion == null) {
-						log.error(Messages.UpdateCheckerJob_4);
-						promptUserError(ERROR_MESSAGE, ERROR_TITLE);
-						return Status.CANCEL_STATUS;
-					}
-				}
-			}
-			if (updateCount > 0) {
-				return Status.OK_STATUS; // OK if there are updates
-			}
+			return checkIfUpdatesAvailable(monitor);
 		} catch (Exception e) {
 			/**
 			 * We are catching the run time exception here since a runtime
@@ -131,8 +100,45 @@ public class UpdateMetaFileReaderJob extends Job {
 			promptUserError(ERROR_MESSAGE, ERROR_TITLE);
 			return Status.CANCEL_STATUS;
 		}
-		return Status.CANCEL_STATUS;// if no exceptions and no updates to be
-									// installed return cancel
+	}
+
+	private IStatus checkIfUpdatesAvailable(IProgressMonitor monitor) {
+		SubMonitor progress = SubMonitor.convert(monitor, Messages.UpdateCheckerJob_1, SUB_MONITOR_INDEX);
+		Collection<IInstallableUnit> installedWSO2Features = updateManager
+				.getInstalledWSO2Features(progress.newChild(1));
+		BufferedReader buffReader = downloadMetaFile();
+		// read the meta file
+		if (buffReader == null) {
+			promptUserError(Messages.UpdateMetaFileReaderJob_3, ERROR_TITLE);
+			return Status.CANCEL_STATUS;
+		}
+		Map<String, String> availaleDevStudioFeatureVerions = new HashMap<String, String>();
+		availaleDevStudioFeatureVerions = readMetaDataFiletoMap(buffReader);
+		deleteDownloadedTempFile();
+		if (availaleDevStudioFeatureVerions.isEmpty()) {
+			log.error(Messages.UpdateCheckerJob_4);
+			return Status.CANCEL_STATUS;
+		}
+		for (IInstallableUnit iInstallableUnit : installedWSO2Features) {
+			if (availaleDevStudioFeatureVerions.containsKey(iInstallableUnit.getId())) {
+				Version availableVersion = generateVersionFromString(
+						availaleDevStudioFeatureVerions.get(iInstallableUnit.getId()));
+				Version upperLimit = generateUpperLimitforUpdates(iInstallableUnit.getVersion());
+				if (availableVersion != null
+						&& availableVersion.compareTo(iInstallableUnit.getVersion()) == AVAILABLE_VERSION_GREATER
+						&& availableVersion.compareTo(upperLimit) == AVAILABLE_VERSION_LESS) {
+					updateCount = updateCount + 1;
+				} else if (availableVersion == null) {
+					log.error(Messages.UpdateCheckerJob_4);
+					promptUserError(ERROR_MESSAGE, ERROR_TITLE);
+					return Status.CANCEL_STATUS;
+				}
+			}
+		}
+		if (updateCount > 0) {
+			return Status.OK_STATUS; // OK if there are updates
+		}
+		return Status.CANCEL_STATUS;
 	}
 
 	private Version generateUpperLimitforUpdates(Version version) {
@@ -150,9 +156,13 @@ public class UpdateMetaFileReaderJob extends Job {
 		File downloadedMetaFile = new File(fileLoc);
 		File downloadedFolderLoc = new File(folderLoc);
 		if (downloadedMetaFile.exists()) {
-			downloadedMetaFile.delete();
+			if (downloadedMetaFile.delete()) {
+				log.error("error in deleting temp file " + downloadedMetaFile);
+			}
 			if (downloadedFolderLoc.exists()) {
-				downloadedFolderLoc.delete();
+				if (downloadedFolderLoc.delete()) {
+					log.error("error in deleting temp folder Location " + downloadedFolderLoc);
+				}
 			}
 		}
 	}
@@ -170,24 +180,25 @@ public class UpdateMetaFileReaderJob extends Job {
 		return availableVersion;
 	}
 
-	private HashMap<String, String> readMetaDataFiletoMap() {
+	private HashMap<String, String> readMetaDataFiletoMap(BufferedReader br) {
 		Map<String, String> featureMap = new HashMap<String, String>();
-		try (BufferedReader br = new BufferedReader(new FileReader(fileLoc))) {
-			for (String line; (line = br.readLine()) != null;) {
-				if (line.contains(",")) {
-					String[] feature = line.split(",");
-					featureMap.put(feature[0] + FEATURE_GROUP_APPEND, feature[1]);
+			try {
+				for (String line; (line = br.readLine()) != null;) {
+					if (line.contains(",")) {
+						String[] feature = line.split(",");
+						featureMap.put(feature[0] + FEATURE_GROUP_APPEND, feature[1]);
+					}
 				}
+			} catch (IOException e) {
+				log.error(Messages.UpdateCheckerJob_3, e);
 			}
-		} catch (IOException e) {
-			log.error(Messages.UpdateCheckerJob_3, e);
-		}
 		return (HashMap<String, String>) featureMap;
 	}
 
-	private boolean downloadMetaFile() {
+	private BufferedReader downloadMetaFile() {
+		BufferedReader reader = null;
+		File updateFile = new File(fileLoc);
 		try {
-			File updateFile = new File(fileLoc);
 			// needs to enable once the pref store values are up
 			IPreferenceStore preferenceStore = PlatformUI.getPreferenceStore();
 			String url = preferenceStore.getString(PreferenceConstants.UPDATE_SITE_URL);
@@ -200,11 +211,12 @@ public class UpdateMetaFileReaderJob extends Job {
 			}
 			URL link = new URL(url + UPDATES_TXT_FILE);
 			FileUtils.copyURLToFile(link, updateFile, TIME_OUT_MS, TIME_OUT_MS);
-			return true;
+			reader = new BufferedReader(new FileReader (updateFile));
+			
 		} catch (IOException e) {
 			log.error("error in writing the the content from the downloaded meta data file for updates", e);
-			return false;
 		}
+		return reader;
 	}
 
 	public static void promptUserError(final String message, final String title) {

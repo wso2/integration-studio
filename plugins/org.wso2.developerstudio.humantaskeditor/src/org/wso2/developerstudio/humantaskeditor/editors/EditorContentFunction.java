1f59c9d7d49d0c0582fae1f43f827b6a27fd2cdb
drifftr <drifftr@gmail.com>
2016-08-01 01:23:49 +0530

    formatting code

--- plugins/org.wso2.developerstudio.humantaskeditor/src/org/wso2/developerstudio/humantaskeditor/editors/EditorContentFunction.java
+++ plugins/org.wso2.developerstudio.humantaskeditor/src/org/wso2/developerstudio/humantaskeditor/editors/EditorContentFunction.java
@@ -65,7 +65,7 @@ public class EditorContentFunction implements AbstractEditorFunctionExecutor {
                 if (!file.exists()) {
                     return "No File";
                 } else {
-                    try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                         String sCurrentLine;
 
                         while ((sCurrentLine = reader.readLine()) != null) {
@@ -122,17 +122,15 @@ public class EditorContentFunction implements AbstractEditorFunctionExecutor {
             ErrorDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
                     (String) parameters[1], null, editorStatus);
             return null;
-        }else if (functionName.equals(HumantaskEditorConstants.JS_CUSTOMFUNC_SAVEPREF)) { // (savepreference,"preferencename","preferencevalue")
-            Preferences preferences = InstanceScope.INSTANCE
-                    .getNode(HumantaskEditorConstants.PLUGIN_ID);
+        } else if (functionName.equals(HumantaskEditorConstants.JS_CUSTOMFUNC_SAVEPREF)) { // (savepreference,"preferencename","preferencevalue")
+            Preferences preferences = InstanceScope.INSTANCE.getNode(HumantaskEditorConstants.PLUGIN_ID);
             Preferences projectNode = preferences.node(getProjectName());
             projectNode.put((String) parameters[1], (String) parameters[2]);
             return null;
-        }else if (functionName.equals(HumantaskEditorConstants.JS_CUSTOMFUNC_GETPREF)) { // (getpreference,"preferencename")
-            Preferences preferences = InstanceScope.INSTANCE
-                    .getNode(HumantaskEditorConstants.PLUGIN_ID);
+        } else if (functionName.equals(HumantaskEditorConstants.JS_CUSTOMFUNC_GETPREF)) { // (getpreference,"preferencename")
+            Preferences preferences = InstanceScope.INSTANCE.getNode(HumantaskEditorConstants.PLUGIN_ID);
             Preferences projectNode = preferences.node(getProjectName());
-            return projectNode.get((String) parameters[1],null);
+            return projectNode.get((String) parameters[1], null);
         } else {
             return null;
         }

51bb8fa5cfc325ea9f86b60b036eb117bfb0aa25
Sharon David <srsharondavid@gmail.com>
2017-09-06 17:24:29 +0530

    updating font pack, and updating icons properties in the source code

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/CallMediatorEditPart.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/CallMediatorEditPart.java
@@ -128,7 +128,7 @@ public class CallMediatorEditPart extends SingleCompartmentComplexFiguredAbstrac
 	 * @generated NOT
 	 */
 	protected IFigure createNodeShape() {
-		return primaryShape = new CallMediatorFigure(new Color(null, 232, 243, 249), new Color(null, 41, 128, 185)) {
+		return primaryShape = new CallMediatorFigure(new Color(null, 245, 252, 255), new Color(null, 41, 128, 185)) {
 			public void setBounds(org.eclipse.draw2d.geometry.Rectangle rect) {
 				super.setBounds(rect);
 				if ((!connected) && (this.getBounds().getLocation().x != 0 && this.getBounds().getLocation().y != 0)) {

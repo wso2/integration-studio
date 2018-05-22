53db7f6d3f38527c0ab77fee48bb6cb4638eb7fb
sohaniwso2 <sohani@wso2.com>
2018-05-22 13:29:41 +0530

    Update installed distributions
    This updates the installed distribution configurations

--- installed-distribution/scripts/installer-script-oxygen.sh
+++ installed-distribution/scripts/installer-script-oxygen.sh
@@ -1,5 +1,6 @@
 wget http://product-dist.wso2.com/p2/developer-studio-kernel/eclipse/oxygen3a/eclipse-jee-oxygen-3a-linux-gtk-x86_64.tar.gz
 wget http://product-dist.wso2.com/p2/developer-studio-kernel/eclipse/oxygen3a/eclipse-jee-oxygen-3a-linux-gtk.tar.gz
+wget http://product-dist.wso2.com/p2/developer-studio-kernel/eclipse/oxygen3a/eclipse-jee-oxygen-3a-macosx-cocoa-x86_64.tar.gz
 wget http://product-dist.wso2.com/p2/developer-studio-kernel/eclipse/oxygen3a/eclipse-jee-oxygen-3a-win32-x86_64.zip
 wget http://product-dist.wso2.com/p2/developer-studio-kernel/eclipse/oxygen3a/eclipse-jee-oxygen-3a-win32.zip
 mvn install:install-file \
@@ -16,6 +17,14 @@ mvn install:install-file \
   -Dpackaging=tar.gz \
   -Dversion=4.7.3a \
   -Dfile=eclipse-jee-oxygen-3a-linux-gtk.tar.gz \
+  -DgeneratePom=true\
+  -DlocalRepositoryPath=$1
+mvn install:install-file \
+  -DgroupId=org.eclipse \
+  -DartifactId=eclipse-jee-oxygen-3a-macosx-cocoa-x86_64 \
+  -Dpackaging=tar.gz \
+  -Dversion=4.7.3a \
+  -Dfile=eclipse-jee-oxygen-3a-macosx-cocoa-x86_64.tar.gz \
   -DgeneratePom=true\
   -DlocalRepositoryPath=$1
 mvn install:install-file \

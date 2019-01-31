f98143da187f9672c06882233ac7ac52930146c2
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2019-01-31 19:43:43 +0530

    add property mediator

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/PropertyMediatorItemProvider.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/PropertyMediatorItemProvider.java
@@ -483,15 +483,15 @@ public class PropertyMediatorItemProvider extends MediatorItemProvider {
     }
 
     /**
-     * This returns PropertyMediator.gif.
+     * This returns PropertyMediator.png.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * @generated NOT
      */
 
     @Override
     public Object getImage(Object object) {
-        return overlayImage(object, getResourceLocator().getImage("full/obj16/PropertyMediator"));
+        return overlayImage(object, getResourceLocator().getImage("full/obj16/PropertyMediator.png"));
     }
 
     /**

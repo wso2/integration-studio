798c7cffb94a5abf533f85805af1c98e0f4ffab0
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2019-01-31 17:02:34 +0530

    add drop mediator

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/DropMediatorEditPart.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/DropMediatorEditPart.java
@@ -125,7 +125,7 @@ public class DropMediatorEditPart extends FixedSizedAbstractMediator {
      * @generated NOT
      */
     protected IFigure createNodeShape() {
-        return primaryShape = new DropMediatorFigure(new Color(null, 41, 128, 185)) {
+        return primaryShape = new DropMediatorFigure(new Color(null, 255, 255, 255)) {
             public void setBounds(org.eclipse.draw2d.geometry.Rectangle rect) {
                 super.setBounds(rect);
                 if (this.getBounds().getLocation().x != 0 && this.getBounds().getLocation().y != 0) {
--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/DropMediatorItemProvider.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/DropMediatorItemProvider.java
@@ -92,15 +92,15 @@ public class DropMediatorItemProvider extends MediatorItemProvider {
     }
 
     /**
-     * This returns DropMediator.gif.
+     * This returns DropMediator.png.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * @generated NOT
      */
 
     @Override
     public Object getImage(Object object) {
-        return overlayImage(object, getResourceLocator().getImage("full/obj16/DropMediator"));
+        return overlayImage(object, getResourceLocator().getImage("full/obj16/DropMediator.png"));
     }
 
     /**

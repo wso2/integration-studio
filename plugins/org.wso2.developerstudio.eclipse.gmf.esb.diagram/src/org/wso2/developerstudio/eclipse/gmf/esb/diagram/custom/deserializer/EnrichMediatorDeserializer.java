b57a0ace9283eab73a1335368170220df14d343b
prabushi <prabushisamarakoon@gmail.com>
2019-03-11 12:28:20 +0530

    Fix product-ei#3318
    Supports saving a JSON inline source with enrich mediator.
    Fix https://github.com/wso2/product-ei/issues/3318

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/custom/deserializer/EnrichMediatorDeserializer.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/custom/deserializer/EnrichMediatorDeserializer.java
@@ -18,6 +18,8 @@ package org.wso2.developerstudio.eclipse.gmf.esb.diagram.custom.deserializer;
 
 import java.util.Map;
 
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMText;
 import org.apache.synapse.mediators.AbstractMediator;
 import org.apache.synapse.mediators.elementary.Source;
 import org.apache.synapse.mediators.elementary.Target;
@@ -97,7 +99,12 @@ public class EnrichMediatorDeserializer extends AbstractEsbNodeDeserializer<Abst
                 if (source.getInlineOMNode() != null) {
 
                     // vishualEnrich.setSourceXML(source.getInlineOMNode().toString());
-                    executeSetValueCommand(ENRICH_MEDIATOR__SOURCE_XML, source.getInlineOMNode().toString());
+                    if (source.getInlineOMNode() instanceof OMElement) {
+                        executeSetValueCommand(ENRICH_MEDIATOR__SOURCE_XML, source.getInlineOMNode().toString());
+                    } else if (source.getInlineOMNode() instanceof OMText) {
+                        executeSetValueCommand(ENRICH_MEDIATOR__SOURCE_XML,
+                                ((OMText) source.getInlineOMNode()).getText());
+                    }
                     executeSetValueCommand(ENRICH_MEDIATOR__INLINE_TYPE, EnrichSourceInlineType.CONTENT);
                 } else if (source.getInlineKey() != null) {
                     executeSetValueCommand(visualEnrichMediator.getInlineRegistryKey(),

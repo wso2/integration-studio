79db4e1e005a1a52c4b7dac583fc441a735745a2
rosensilva <rosen@live.com>
2019-03-18 22:45:32 +0530

    add eef properties grouping for switch mediator and xslt mediator

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src-gen/org/wso2/developerstudio/eclipse/gmf/esb/components/XSLTMediatorPropertiesEditionComponent.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src-gen/org/wso2/developerstudio/eclipse/gmf/esb/components/XSLTMediatorPropertiesEditionComponent.java
@@ -62,6 +62,7 @@ import org.wso2.developerstudio.eclipse.gmf.esb.XSLTResource;
 import org.wso2.developerstudio.eclipse.gmf.esb.impl.EsbFactoryImpl;
 import org.wso2.developerstudio.eclipse.gmf.esb.parts.EsbViewsRepository;
 import org.wso2.developerstudio.eclipse.gmf.esb.parts.XSLTMediatorPropertiesEditionPart;
+import org.wso2.developerstudio.eclipse.gmf.esb.presentation.EEFPropertyViewUtil;
 
 
 // End of user code
@@ -558,7 +559,13 @@ public class XSLTMediatorPropertiesEditionComponent extends SinglePartProperties
 	}
 
 
-	
+    /**
+     * @generated NOT
+     */
+    @Override
+    public String getHelpContent(Object key, int kind) {
+        return EEFPropertyViewUtil.getHelpContent(key);
+    }
 
 	
 

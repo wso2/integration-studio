c8202c3d5619fde7472b1ac293f7aadef1b482d1
Dileesha <dilee.dev@gmail.com>
2019-03-18 11:12:48 +0530

    Refactor BAM Mediator properties

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src-gen/org/wso2/developerstudio/eclipse/gmf/esb/components/BAMMediatorPropertiesEditionComponent.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src-gen/org/wso2/developerstudio/eclipse/gmf/esb/components/BAMMediatorPropertiesEditionComponent.java
@@ -41,6 +41,7 @@ import org.wso2.developerstudio.eclipse.gmf.esb.EsbPackage;
 
 import org.wso2.developerstudio.eclipse.gmf.esb.parts.BAMMediatorPropertiesEditionPart;
 import org.wso2.developerstudio.eclipse.gmf.esb.parts.EsbViewsRepository;
+import org.wso2.developerstudio.eclipse.gmf.esb.presentation.EEFPropertyViewUtil;
 
 
 // End of user code
@@ -312,10 +313,16 @@ public class BAMMediatorPropertiesEditionComponent extends SinglePartPropertiesE
 		}
 		return ret;
 	}
-
-
 	
+    // Start of user code for help compatibility
 
-	
+    /**
+     * @generated NOT
+     */
+    @Override
+    public String getHelpContent(Object key, int kind) {
+        return EEFPropertyViewUtil.getHelpContent(key);
+    }
 
+    // End of user code
 }

f8cafc2edce06aa1c31a497083d69bd10d0595f5
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2019-04-01 19:02:41 +0530

    fix for issue #3374 on APIs
    fix for https://github.com/wso2/product-ei/issues/3374

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/APIResourceInputConnectorEditPart.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/APIResourceInputConnectorEditPart.java
@@ -324,7 +324,7 @@ public class APIResourceInputConnectorEditPart extends AbstractBaseFigureInputCo
             layoutThis.numColumns = 1;
             layoutThis.makeColumnsEqualWidth = true;
             layoutThis.marginHeight = 0;
-            layoutThis.marginWidth = 0;
+            layoutThis.marginWidth = 1;
             this.setLayoutManager(layoutThis);
 
             this.setCornerDimensions(new Dimension(1, 1));

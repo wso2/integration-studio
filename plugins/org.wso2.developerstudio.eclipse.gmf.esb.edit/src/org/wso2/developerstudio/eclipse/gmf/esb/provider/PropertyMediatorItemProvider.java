923aba666fe8d7b8d225f78fc98ef1e7e70cd5fe
Dinuksha <dinuksha.ishwari@gmail.com>
2019-04-01 20:18:44 +0530

    Remove unused variables

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/PropertyMediatorItemProvider.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/PropertyMediatorItemProvider.java
@@ -509,9 +509,7 @@ public class PropertyMediatorItemProvider extends MediatorItemProvider {
         PropertyName labelValue = ((PropertyMediator)object).getPropertyName();
         String newPropertyName = ((PropertyMediator)object).getNewPropertyName();
         String label = labelValue == null ? null : labelValue.toString();
-        String value = ((PropertyMediator)object).getValue();
         String newLabel = label == "New Property..." ? newPropertyName : label ;
-        String propertyName = WordUtils.abbreviate(newLabel.toString(), 8, 30, " ...");
 
         return newLabel == null || label.length() == 0 ?
             getString("_UI_PropertyMediator_type") :

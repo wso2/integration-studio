08c9971ca1b74c31a740bb4f600841f95c6dbc92
Praveen Nadarajah <n.praveen.rc@gmail.com>
2019-04-03 16:59:47 +0530

    Add generated NOT to required method

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src-gen/org/wso2/developerstudio/eclipse/gmf/esb/parts/forms/RuleFactPropertiesEditionPartForm.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src-gen/org/wso2/developerstudio/eclipse/gmf/esb/parts/forms/RuleFactPropertiesEditionPartForm.java
@@ -106,6 +106,7 @@ public class RuleFactPropertiesEditionPartForm extends SectionPropertiesEditingP
 	 * @see org.eclipse.emf.eef.runtime.api.parts.IFormPropertiesEditionPart#
 	 *  createFigure(org.eclipse.swt.widgets.Composite, org.eclipse.ui.forms.widgets.FormToolkit)
 	 * 
+	 * @generated NOT
 	 */
 	public Composite createFigure(final Composite parent, final FormToolkit widgetFactory) {
 		ScrolledForm scrolledForm = widgetFactory.createScrolledForm(parent);

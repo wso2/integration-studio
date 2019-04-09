9b41b9de3a85099e502909a1e016bdce876efa05
Praveen Nadarajah <n.praveen.rc@gmail.com>
2019-04-09 15:20:10 +0530

    Add generated NOT to required method

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/MessageBuilderItemProvider.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/MessageBuilderItemProvider.java
@@ -143,7 +143,7 @@ public class MessageBuilderItemProvider extends EsbNodeItemProvider {
      * This returns the label text for the adapted class.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * @generated NOT
      */
     @Override
     public String getText(Object object) {

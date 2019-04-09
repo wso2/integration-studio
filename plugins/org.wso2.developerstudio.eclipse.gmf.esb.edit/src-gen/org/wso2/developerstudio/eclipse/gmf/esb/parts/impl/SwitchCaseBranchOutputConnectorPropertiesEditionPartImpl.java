c51bd2bfd0101af256d9637a2b6adad1f260ae80
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2019-04-09 15:53:25 +0530

    fix issue #3561
    fix issue https://github.com/wso2/product-ei/issues/3561

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src-gen/org/wso2/developerstudio/eclipse/gmf/esb/parts/impl/SwitchCaseBranchOutputConnectorPropertiesEditionPartImpl.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.edit/src-gen/org/wso2/developerstudio/eclipse/gmf/esb/parts/impl/SwitchCaseBranchOutputConnectorPropertiesEditionPartImpl.java
@@ -202,13 +202,13 @@ public class SwitchCaseBranchOutputConnectorPropertiesEditionPartImpl extends Co
             @Override
             @SuppressWarnings("synthetic-access")
             public void keyReleased(KeyEvent e) {
-                if (!EEFPropertyViewUtil.isReservedKeyCombination(e)) {
-                    if (propertiesEditionComponent != null)
-                        propertiesEditionComponent.firePropertiesChanged(new PropertiesEditionEvent(
-                                SwitchCaseBranchOutputConnectorPropertiesEditionPartImpl.this,
-                                EsbViewsRepository.SwitchCaseBranchOutputConnector.Properties.caseRegex,
-                                PropertiesEditionEvent.COMMIT, PropertiesEditionEvent.SET, null, caseRegex.getText()));
-                }
+              
+                if (propertiesEditionComponent != null)
+                    propertiesEditionComponent.firePropertiesChanged(new PropertiesEditionEvent(
+                            SwitchCaseBranchOutputConnectorPropertiesEditionPartImpl.this,
+                            EsbViewsRepository.SwitchCaseBranchOutputConnector.Properties.caseRegex,
+                            PropertiesEditionEvent.COMMIT, PropertiesEditionEvent.SET, null, caseRegex.getText()));
+                
             }
 
             @Override

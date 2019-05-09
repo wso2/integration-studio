ac5771d49ae86565e300271b26a3b21770ce85a8
prabushi <prabushisamarakoon@gmail.com>
2019-05-09 15:41:47 +0530

    Fix product-ei#3937
    Fix https://github.com/wso2/product-ei/issues/3937

--- plugins/org.wso2.developerstudio.eclipse.utils/src/org/wso2/developerstudio/eclipse/utils/constants/ProjectConstants.java
+++ plugins/org.wso2.developerstudio.eclipse.utils/src/org/wso2/developerstudio/eclipse/utils/constants/ProjectConstants.java
@@ -20,6 +20,7 @@ public class ProjectConstants {
 	public static final String JAVA_NATURE_ID="org.eclipse.jdt.core.javanature";
 	public static final String AXIS2_NATURE_ID="org.wso2.developerstudio.eclipse.axis2.project.nature";
 	public static final String DSS_NATURE_ID="org.wso2.developerstudio.eclipse.ds.project.nature";
+	public static final String DATASOURCE_NATURE_ID="org.wso2.developerstudio.eclipse.datasource.project.nature";
 	public static final String BPS_NATURE_ID="org.wso2.developerstudio.eclipse.bpel.project.nature";
 	public static final String BRS_NATURE_ID="org.wso2.developerstudio.eclipse.artifact.businessruleservice.project.nature";
 }

88a00e396d32b14b4a78891f386ceba8c3627b40
sajithaliyanage <sajithaliyanage@gmail.com>
2019-11-30 15:36:28 +0530

    Create new wizards for Docker and Kubernetes projects to deal with composite projects

--- plugins/org.wso2.developerstudio.eclipse.docker.distribution/src/org/wso2/developerstudio/eclipse/docker/distribution/ui/wizard/KubernetesProjectCreationWizard.java
+++ plugins/org.wso2.developerstudio.eclipse.docker.distribution/src/org/wso2/developerstudio/eclipse/docker/distribution/ui/wizard/KubernetesProjectCreationWizard.java
@@ -47,15 +47,28 @@ public class KubernetesProjectCreationWizard extends Wizard implements IExportWi
     private MavenDetailsPage mavenDetailPage;
     private DockerModel dockerModel;
     
+    public KubernetesProjectCreationWizard() {
+        dockerModel = new DockerModel();
+    }
+    
+    public KubernetesProjectCreationWizard(boolean isCompositeOnClickCreation) {
+        dockerModel = new DockerModel();
+        dockerModel.setCompositeOnClickContainerCreation(isCompositeOnClickCreation);
+    }
+    
     @Override
     public void init(IWorkbench workbench, IStructuredSelection selection) {
         setWindowTitle(WINDOW_TITLE);
-        dockerModel = new DockerModel();
         try {
             IProject project = getSelectedProject(selection);
             File location;
             if (project != null) {
-                location = project.getLocation().toFile();
+                if (dockerModel.isCompositeOnClickContainerCreation()) {
+                    location = project.getParent().getLocation().toFile();
+                    dockerModel.setSelectedCompositeProjectOnCreation(project);
+                } else {
+                    location = project.getLocation().toFile();
+                }
             } else {
                 location = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
             }

c212fb199f8cace98ce22621b4f0ec12684674db
prabushi <prabushisamarakoon@gmail.com>
2020-01-17 17:47:49 +0530

    Add operation related funtionality
    Add dss operation related functionality for new editor.

--- plugins/org.wso2.developerstudio.eclipse.ds.editor/DSSEditor/assets/js/data-handler.js
+++ plugins/org.wso2.developerstudio.eclipse.ds.editor/DSSEditor/assets/js/data-handler.js
@@ -13,6 +13,7 @@ $(document).ready(function ($) {
         populateGeneralDetails(root);
         populateTransportSettings(root);
         populateDataSources(root);
+        populateOperations(root);
 
     });
 
@@ -80,8 +81,54 @@ s
         save(root, url);
     });
     // End of input event handlers - General details
-
     /** End of Event handlers **/
+    
+    // Start of Operations
+    
+    /**
+     * Populates query elements and set enableStreaming upon operation modal opening.
+     */
+    $('#o-operation-add-btn').click(function() {
+    	populateQueriesForOperationForm(root);
+    	$('#o-addedit-enablestreaming-checkbox').prop("checked", true);
+    });
+    
+    /**
+     * Updates root content with operation details upon modal save.
+     */
+    $("#operation-form-save-btn").click(function (e) {
+        e.preventDefault();
+        addOperation(root);
+    });
+    
+    /**
+     * Deletes operation table entry and root entry upon delete.
+     */
+    $(document).on('click','#o-operation-table .fa-trash',function() {
+    	let tds = $(this).closest("tr").find('td');
+    	deleteOperation(root, tds[0].innerText);
+    	$(this).closest("tr").remove();
+    });
+    
+    /**
+     * Populates query parameters upon query id changes.
+     */
+    $('#o-addedit-queryid-select').change(function(e) {
+    	e.preventDefault();
+    	let queryid = $("#o-addedit-queryid-select").val(); 
+    	populateOperationParamaterTable(root, queryid);
+    });
+    
+    /**
+     * Populates operation modal upon edit option.
+     */
+    $(document).on('click','#o-operation-table .fa-edit',function() {
+    	let tds = $(this).closest("tr").find('td');
+    	populateOperationForm(root, tds[0].innerText, tds[1].innerText);
+    });
+    
+    // End of Operations
+
 
 });
 
@@ -381,3 +428,249 @@ function save(root, url) {
     return request;
 }
 
+/**
+ * Processes the add operation.
+ *
+ * @param root Document object
+ */
+function addOperation(root) {
+    // Check if operation name already exists
+    let operations = root.getElementsByTagName("operation");
+    let new_operation_name = $("#o-addedit-opname-input").val();
+    
+    for (let i = 0, len = operations.length; i < len; i++) {
+        if (operations[i].attributes.getNamedItem("name").value == new_operation_name) {
+        	showOperationNotification("danger", "An operation with the given name already exists.", 4000);
+            return false;
+        }
+    }
+
+    // Process operation modal data
+    let formData = $("#operation-form").find(':visible').serializeArray();
+    let dataObj = {};
+
+    $(formData).each(function(i, field){
+        dataObj[field.name] = field.value;
+    });
+
+    processOperationInputData(root, dataObj, true);
+    return true;
+}
+
+/**
+ * Shows an alert of a given type in the operation editor modal.
+ *
+ * @param type Type of the alert: success | info | warning | danger
+ * @param message Message to be displayed.
+ * @param interval Number of milliseconds before the notification disappears. If not provided or '0',
+ * notification will stay forever.
+ */
+function showOperationNotification(type, message, interval) {
+    let alertHtml = "<div id='operation-notification-alert' class=\"alert " + "alert-" + type + "\"" + ">" + message + "</div>";
+    $("#operation-notification-alert-holder").html(alertHtml);
+    $("#operation-notification-alert").show();
+
+    if (interval > 0) {
+        window.setTimeout(function () {
+            $("#operation-notification-alert").fadeTo(500, 0).slideUp(500, function() {
+                $(this).remove();
+            });
+        }, interval);
+    }
+}
+
+/**
+ * Processes form input data of the create/edit operation form.
+ *
+ * @param root Document root element.
+ * @param data Form data in data['field-name'] format.
+ * @param deleteIfExists Delete the operation node if exists already.
+ */
+function processOperationInputData(root, data, deleteIfExists) {
+    let dataRoot = root.getElementsByTagName("data")[0];
+    let operations = root.getElementsByTagName("operation");
+    let new_operation_name = $("#o-addedit-opname-input").val();
+
+    // Deletes if operation node exists
+    if (deleteIfExists) {
+        for (let i = 0, len = operations.length; i < len; i++) {
+            if (operations[i].name == new_operation_name) {
+                // Delete the node.
+                root.documentElement.removeChild(operations[i]);
+                return;
+            }
+        }
+    }
+
+    // Creates a new operation element
+    let operationElement = root.createElement("operation");
+    operationElement.setAttribute("name", new_operation_name);
+    
+    //Creates description element
+    let description = $('#o-addedit-description-input').val();
+    let descriptionElement = root.createElement("description");
+    let text_node = root.createTextNode(description);
+    descriptionElement.appendChild(text_node);
+    operationElement.appendChild(descriptionElement);
+    
+    //Creates a new call-query element
+    let callqueryElement = root.createElement("call-query");
+    let query_id = $('#o-addedit-queryid-select').val();
+    callqueryElement.setAttribute("href", query_id);
+    
+    //Creates with-param elements
+    let op_parameters = $('#operation-param-table > tbody').find('tr');
+    $.each(op_parameters, function(index, op_parameter) {
+		let withparamElement = root.createElement("with-param");
+		withparamElement.setAttribute("name", op_parameter.cells[0].firstChild.value);
+		withparamElement.setAttribute("query-param", op_parameter.cells[1].firstChild.value);
+		callqueryElement.appendChild(withparamElement);
+	});
+    
+    //Creates Enable Stream element
+    if (!$('#o-addedit-enablestreaming-checkbox').is(":checked")) {
+    	operationElement.setAttribute("disableStreaming", true);
+    }
+    
+    //Creates Return request status element
+    if ($('#o-addedit-returnreqstatus-checkbox').is(":checked")) {
+    	operationElement.setAttribute("returnRequestStatus", true);
+    }
+    
+    operationElement.appendChild(callqueryElement);
+    
+    //Appends to operations table
+    dataRoot.appendChild(operationElement);
+        
+    //Adds a row to operations table
+    let op_row = '<tr><td>' + new_operation_name + '</td><td>' + query_id + '</td><td class="text-center"><i class="fa fa-edit"></i><i class="fa fa-trash"></i></td></tr>';
+	$('#o-operation-table > tbody').append(op_row);
+	$('#o-operation-addedit-modal').removeClass('show');
+	$('.modal-backdrop.fade.show').removeClass('show');
+	clearOperationForm();
+}
+
+/**
+ * Populates operation modal data.
+ * 
+ * @param root Root document
+ * @param operation_name Operation name
+ * @param query_name Query id
+ */
+function populateOperationForm(root, operation_name, query_name) {
+	let operations = root.getElementsByTagName("operation");
+	
+	$.each(operations, function (index, op) {
+		let op_name = op.attributes.getNamedItem("name").value;
+		
+		if (op_name == operation_name) {
+			let call_query = op.getElementsByTagName('call-query');
+			let q_id = call_query[0].attributes.getNamedItem("href").value;
+			
+			if (q_id == query_name) {
+				$('#o-addedit-opname-input').val(op_name);
+				$('#o-addedit-description-input').val(op.getElementsByTagName('description')[0].textContent);
+				populateQueriesForOperationForm(root);
+				$('#o-addedit-queryid-select').val(q_id);
+				populateOperationParamaterTable(root, q_id);
+				
+				let disableStreaming = op.attributes.getNamedItem("disableStreaming").value;
+				if (disableStreaming == "true") {
+					$('#o-addedit-enablestreaming-checkbox').prop( "checked", false );
+				} else {
+					$('#o-addedit-enablestreaming-checkbox').prop( "checked", true );
+				}
+				
+				let returnRequestStatus = op.attributes.getNamedItem("returnRequestStatus").value;
+				if (returnRequestStatus == "true") {
+					$('#o-addedit-returnreqstatus-checkbox').prop( "checked", true );
+				} else {
+					$('#o-addedit-returnreqstatus-checkbox').prop( "checked", false );
+				}
+				
+				$("#o-operation-addedit-modal").modal('show');
+			}
+		}
+	});
+}
+
+/**
+ * Populates operation entries to operations table.
+ * 
+ * @param root Root document
+ */
+function populateOperations(root) {
+	$('#o-operation-table').find('tbody').find('tr').detach();
+	let operations = root.getElementsByTagName("operation");
+	
+	$.each(operations, function (index, op) {
+		let op_name = op.attributes.getNamedItem("name").value;
+		let call_query = op.getElementsByTagName('call-query');
+		let q_id = call_query[0].attributes.getNamedItem("href").value;
+		let op_row = '<tr><td>' + op_name + '</td><td>' + q_id + '</td><td class="text-center"><i class="fa fa-edit"></i><i class="fa fa-trash"></i></td></tr>';
+		$('#o-operation-table > tbody').append(op_row);
+	});
+}
+
+/**
+ * Populates query list.
+ * 
+ * @param root Root document
+ */
+function populateQueriesForOperationForm(root) {
+	let select = $('#o-addedit-queryid-select');
+	select.find("option").detach();
+	let option = new Option("-- Select Query --", "", true, "");
+	select.append(option);
+	let queries = root.getElementsByTagName("query");
+	
+	$.each(queries, function (index, query) {
+		let query_id = query.attributes.getNamedItem("id").value;
+		select.append(new Option(query_id));
+	});
+}
+
+/**
+ * Populates operation parameters table.
+ * 
+ * @param root Root document
+ * @param queryid Query id
+ */
+function populateOperationParamaterTable(root, queryid) {
+		
+	$('#operation-param-table').find('tbody').find('tr').detach();
+	$('#o-addedit-returnreqstatus-checkbox').prop("checked", false);
+	
+	if (queryid != "") {
+		$('#operation-query-param-area').toggle(true);
+		$('#op-table').collapse("show");
+    	
+    	let queries = root.getElementsByTagName("query");
+    	for (let i = 0, len = queries.length; i < len; i++) {
+    		var query_id = queries[i].attributes.getNamedItem("id").value;
+    		
+    		if (queryid == query_id) {
+    			let params = queries[i].getElementsByTagName("param");
+    			
+            	if (params.length > 0) {
+            		$('#returnreqstatus-area').toggle(true);
+            	} else {
+            		$('#returnreqstatus-area').toggle(false);
+            		$('#o-addedit-returnreqstatus-checkbox').prop("checked", false);
+            	}
+            	
+        		$.each(params, function (i, param) {
+        			let param_name = param.attributes.getNamedItem("name").value;
+        			let row = "<tr><td><input type='text' placeholder='Query param name' class='form-control' value='" + param_name 
+        				+ "' style='width: 100%;' /></td><td><input type='text' placeholder='Operation param name' value='" + param_name 
+        				+ "' class='form-control' style='width: 100%;'/></td><td class='text-center'><i class='fa fa-trash'></i></td></tr>";
+        			$('#operation-param-table > tbody').append(row);
+        			
+        		});
+    		}
+    	}	
+	} else {
+		$('#operation-query-param-area').toggle(false);
+		$('#returnreqstatus-area').toggle(false);
+	}
+}

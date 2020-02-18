6a2f3a88bbd9a91d427e0b667fcb3de0f436e8a8
prabushi <prabushisamarakoon@gmail.com>
2020-02-18 17:22:02 +0530

    Enable xsd validation
    Enable xsd validation for ds editor.

--- plugins/org.wso2.developerstudio.eclipse.ds.editor/src/org/wso2/developerstudio/eclipse/ds/editors/DSSMultiPageEditor.java
+++ plugins/org.wso2.developerstudio.eclipse.ds.editor/src/org/wso2/developerstudio/eclipse/ds/editors/DSSMultiPageEditor.java
@@ -172,16 +172,16 @@ public class DSSMultiPageEditor extends MultiPageEditorPart implements IResource
         IDocumentProvider dp = editor.getDocumentProvider();
         IDocument doc = dp.getDocument(editor.getEditorInput());
         String sourceContent = doc.get();
-//        String validationMsg = validateDSSConfig(sourceContent);
-//        if (validationMsg.equals("valid")) {
-//            deleteMarkers();
+        String validationMsg = validateDSSConfig(sourceContent);
+        if (validationMsg.equals("valid")) {
+            deleteMarkers();
             setDsXmlContent(sourceContent);
             getEditor(1).doSave(monitor);
             
             ((DSSVisualEditorPage) getEditor(VISUAL_EDITOR_PAGE_INDEX)).getBrowser().refresh();
-//        } else {
-//            addMarker(validationMsg);
-//        }
+        } else {
+            addMarker(validationMsg);
+        }
     }
     
     private void deleteMarkers() {
@@ -221,7 +221,7 @@ public class DSSMultiPageEditor extends MultiPageEditorPart implements IResource
         return null;
     }
     
-    public static String validateDSSConfig(String dssContent){
+    public static String validateDSSConfig(String dssContent) {
         
         try {
             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);

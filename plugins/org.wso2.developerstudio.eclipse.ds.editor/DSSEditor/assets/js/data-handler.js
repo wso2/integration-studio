cdc41df24e0dd0d5cc92aca6dcb57dc15e15c8a7
DMadhuranga <kdanushka8@gmail.com>
2020-03-16 17:30:01 +0530

    Fixed: Issue 792
    Trying to configure the create datasource wizard before clearing the values. Hence configuration is done according to the values in previously created form. Changed the order so that form values are cleared before configuring the wizard.
    
    Related Issue: https://github.com/wso2/devstudio-tooling-ei/issues/792

--- plugins/org.wso2.developerstudio.eclipse.ds.editor/DSSEditor/assets/js/data-handler.js
+++ plugins/org.wso2.developerstudio.eclipse.ds.editor/DSSEditor/assets/js/data-handler.js
@@ -193,8 +193,8 @@ $(document).ready(function ($) {
     });
 
     $("#ds-datasource-add-btn").click(function() {
-        resetDSAddEditModal();
         $("#create-ds-form").trigger('reset');
+        resetDSAddEditModal();
         openDSModal(false);
     });
 

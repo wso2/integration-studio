827370bca7f75da39fafa82b6bed1b359e1d6913
DMadhuranga <kdanushka8@gmail.com>
2020-03-17 14:08:31 +0530

    Adding Support for service status
    Enable support for serviceStatus attribute of the data service.
    
    Related Issue: https://github.com/wso2/devstudio-tooling-ei/issues/795

--- plugins/org.wso2.developerstudio.eclipse.ds.editor/DSSEditor/assets/js/advanced-validation.js
+++ plugins/org.wso2.developerstudio.eclipse.ds.editor/DSSEditor/assets/js/advanced-validation.js
@@ -4,6 +4,7 @@
 $(document).ready(function() {
 	$('#ac-enable-streaming-check').prop("checked", true);
 	$('#ac-enable-batch-check').prop("checked", false);
+	$('#ac-active-service-status').prop("checked", false);
 	$('#ac-enable-boxcarring-check').prop("checked", false);
 	$('#ac-disable-legacy-boxcarring-check').prop("checked", false);
 	$('#ac-disable-legacy-boxcarring-group').toggle(false);
@@ -19,6 +20,13 @@ function populateAdvancedProperties(root) {
 		$('#ac-enable-batch-check').prop("checked", false);
 	}
 	
+	let activeServiceStatus = data.attributes.getNamedItem("serviceStatus");
+	if (activeServiceStatus != null && activeServiceStatus != undefined && activeServiceStatus.value == "active") {
+		$('#ac-active-service-status').prop("checked", true);
+	} else {
+		$('#ac-active-service-status').prop("checked", false);
+	}
+
 	let enableBoxcarring = data.attributes.getNamedItem("enableBoxcarring");
 	if (enableBoxcarring != null && enableBoxcarring != undefined && enableBoxcarring.value == "true") {
 		$('#ac-enable-boxcarring-check').prop("checked", true);

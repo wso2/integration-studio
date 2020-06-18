3f2d2bc339dff6e01c3b03177a4bd26c8a2de605
prabushi <prabushisamarakoon@gmail.com>
2020-06-18 13:06:28 +0530

    Refactor project wizards
    Refactor project creation wizards.
    
    Fix https://github.com/wso2/devstudio-tooling-ei/issues/1073, https://github.com/wso2/devstudio-tooling-ei/issues/1024 and https://github.com/wso2/devstudio-tooling-ei/issues/1025

--- plugins/org.wso2.developerstudio.eclipse.artifact.connector/src/org/wso2/developerstudio/eclipse/artifact/connector/validator/ConnectorFieldsController.java
+++ plugins/org.wso2.developerstudio.eclipse.artifact.connector/src/org/wso2/developerstudio/eclipse/artifact/connector/validator/ConnectorFieldsController.java
@@ -26,7 +26,7 @@ public class ConnectorFieldsController extends AbstractFieldController {
 	public void validate(String modelProperty, Object value,
 			ProjectDataModel model) throws FieldValidationException {
 		if (modelProperty.equals("project.name")) {
-			CommonFieldValidator.validateProjectField(value);
+			CommonFieldValidator.validateProjectField(value, "Connector Exporter");
 		} 
 	}
 

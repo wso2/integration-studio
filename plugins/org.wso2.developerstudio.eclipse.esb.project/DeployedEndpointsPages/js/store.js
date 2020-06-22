f5d6118e5578bf0df0a68903835bb87b7b8b3a2f
prabushi <prabushisamarakoon@gmail.com>
2020-06-22 18:51:26 +0530

    Reload deployed services at hotdeployment
    Reload deployed services view if a car is deployed using the hot deployment feature.
    Fix https://github.com/wso2/devstudio-tooling-ei/issues/1105

--- plugins/org.wso2.developerstudio.eclipse.esb.project/DeployedEndpointsPages/js/store.js
+++ plugins/org.wso2.developerstudio.eclipse.esb.project/DeployedEndpointsPages/js/store.js
@@ -17,26 +17,7 @@ function resolveGetParam(param) {
 
 loadContent();
 
-//getAccessToken();
-//
-//function getAccessToken() {
-//	accessToken = "";
-//	$.ajax({
-//	    url: login_endpoint,
-//	    type: 'get',
-//	    headers: {
-//	        "Authorization": 'Basic YWRtaW46YWRtaW4='  //for object property name, use quoted notation shown in second
-//	    },
-//	    dataType: 'json',
-//	    success: function (data) {
-//	    	accessToken = data.AccessToken;
-//	        loadContent(accessToken);
-//	    }
-//	});
-//	return accessToken;
-//}
-
-function loadContent(){
+function loadContent() {
 	populateAPIList();
 	populateProxyList();
 	populateDataServiceList();
@@ -45,27 +26,6 @@ function loadContent(){
 function populateAPIList() {
 	apijsontemp = JSON.parse(apijson);
 	apiList = apijsontemp.list;
-//	$.ajax({
-//	    url: apis_endpoint,
-//	    type: 'get',
-//	    headers: {
-//	        "Authorization": 'Bearer ' + accessToken  //for object property name, use quoted notation shown in second
-//	    },
-//	    dataType: 'json',
-//	    success: function (data) {
-//	    	apiList = data.list;
-////	    	$.each(apiList, function(i, item) {
-////	    		$table = $('#table-container-api table tbody');
-////	    		$table.append('<tr><td>' + item.name + '</td><td>' + item.url + '</td></tr>');
-////	    	});
-//	    	
-//	    	for(var i = 0; i < apiList.length; i++) {
-//	    	    item = apiList[i];
-//	    	    $table = $('#table-container-api table tbody');
-//	    		$table.append('<tr><td>' + item.name + '</td><td>' + item.url + '</td></tr>');
-//	    	}
-//	    }
-//	});
 	
 	for(var i = 0; i < apiList.length; i++) {
 	    var item = apiList[i];
@@ -99,23 +59,3 @@ function populateDataServiceList() {
 
 	return dataserviceList;
 }
-
-//function populateProxyList() {
-//	proxyList = "";
-//	$.ajax({
-//	    url: proxy_endpoint,
-//	    type: 'get',
-//	    headers: {
-//	        "Authorization": 'Bearer ' + accessToken  //for object property name, use quoted notation shown in second
-//	    },
-//	    dataType: 'jsonp',
-//	    success: function (data) {	    	
-//	    	proxyList = data.list;
-//	    	$.each(proxyList, function(i, item) {
-//	    		$table = $('#table-container-proxy table tbody');
-//	    		$table.append('<tr><td>' + item.name + '</td><td>' + item.wsdl1_1 + '</td><td>' + item.wsdl2_0 + '</td></tr>');
-//	    	});
-//	    }
-//	});
-//	return proxyList;
-//}

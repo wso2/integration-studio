4071a8e96bd64efab44a5f806c44428055f70191
prabushi <prabushisamarakoon@gmail.com>
2020-06-23 10:18:04 +0530

    Change composite module name
    Change composite module name and fix the build failure.

--- plugins/org.wso2.developerstudio.eclipse.distribution.project/src/org/wso2/developerstudio/eclipse/distribution/project/validator/DistributionProjectFieldController.java
+++ plugins/org.wso2.developerstudio.eclipse.distribution.project/src/org/wso2/developerstudio/eclipse/distribution/project/validator/DistributionProjectFieldController.java
@@ -26,7 +26,7 @@ public class DistributionProjectFieldController extends AbstractFieldController 
 	public void validate(String modelProperty, Object value,
 			ProjectDataModel model) throws FieldValidationException {
 		if (modelProperty.equals("project.name")) {
-			CommonFieldValidator.validateProjectField(value, "Composite Application Exporter");
+			CommonFieldValidator.validateProjectField(value, "Composite Exporter");
 		}
 	}
 

20ff50e76968ffe2caa032a00549c8d7a4955be9
Dilan Tharaka <tharakamd.12@cse.mrt.ac.lk>
2020-10-07 07:36:30 +0530

    add remove attribute support to enrich mediator

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb/src/org/wso2/developerstudio/eclipse/gmf/esb/EnrichTargetAction.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb/src/org/wso2/developerstudio/eclipse/gmf/esb/EnrichTargetAction.java
@@ -23,223 +23,248 @@ import org.eclipse.emf.common.util.Enumerator;
  */
 public enum EnrichTargetAction implements Enumerator {
     /**
-     * The '<em><b>REPLACE</b></em>' literal object.
-     * <!-- begin-user-doc -->
+	 * The '<em><b>REPLACE</b></em>' literal object.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #REPLACE_VALUE
-     * @generated
-     * @ordered
-     */
+	 * @see #REPLACE_VALUE
+	 * @generated
+	 * @ordered
+	 */
     REPLACE(0, "REPLACE", "replace"),
 
     /**
-     * The '<em><b>CHILD</b></em>' literal object.
-     * <!-- begin-user-doc -->
+	 * The '<em><b>CHILD</b></em>' literal object.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #CHILD_VALUE
-     * @generated
-     * @ordered
-     */
+	 * @see #CHILD_VALUE
+	 * @generated
+	 * @ordered
+	 */
     CHILD(1, "CHILD", "child"),
 
     /**
-     * The '<em><b>SIBLING</b></em>' literal object.
-     * <!-- begin-user-doc -->
+	 * The '<em><b>SIBLING</b></em>' literal object.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #SIBLING_VALUE
-     * @generated
-     * @ordered
-     */
-    SIBLING(2, "SIBLING", "sibling");
+	 * @see #SIBLING_VALUE
+	 * @generated
+	 * @ordered
+	 */
+    SIBLING(2, "SIBLING", "sibling"), /**
+	 * The '<em><b>REMOVE</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #REMOVE_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	REMOVE(3, "REMOVE", "remove");
 
     /**
-     * The '<em><b>REPLACE</b></em>' literal value.
-     * <!-- begin-user-doc -->
+	 * The '<em><b>REPLACE</b></em>' literal value.
+	 * <!-- begin-user-doc -->
      * <p>
      * If the meaning of '<em><b>REPLACE</b></em>' literal object isn't clear,
      * there really should be more of a description here...
      * </p>
      * <!-- end-user-doc -->
-     * @see #REPLACE
-     * @model literal="replace"
-     * @generated
-     * @ordered
-     */
+	 * @see #REPLACE
+	 * @model literal="replace"
+	 * @generated
+	 * @ordered
+	 */
     public static final int REPLACE_VALUE = 0;
 
     /**
-     * The '<em><b>CHILD</b></em>' literal value.
-     * <!-- begin-user-doc -->
+	 * The '<em><b>CHILD</b></em>' literal value.
+	 * <!-- begin-user-doc -->
      * <p>
      * If the meaning of '<em><b>CHILD</b></em>' literal object isn't clear,
      * there really should be more of a description here...
      * </p>
      * <!-- end-user-doc -->
-     * @see #CHILD
-     * @model literal="child"
-     * @generated
-     * @ordered
-     */
+	 * @see #CHILD
+	 * @model literal="child"
+	 * @generated
+	 * @ordered
+	 */
     public static final int CHILD_VALUE = 1;
 
     /**
-     * The '<em><b>SIBLING</b></em>' literal value.
-     * <!-- begin-user-doc -->
+	 * The '<em><b>SIBLING</b></em>' literal value.
+	 * <!-- begin-user-doc -->
      * <p>
      * If the meaning of '<em><b>SIBLING</b></em>' literal object isn't clear,
      * there really should be more of a description here...
      * </p>
      * <!-- end-user-doc -->
-     * @see #SIBLING
-     * @model literal="sibling"
-     * @generated
-     * @ordered
-     */
+	 * @see #SIBLING
+	 * @model literal="sibling"
+	 * @generated
+	 * @ordered
+	 */
     public static final int SIBLING_VALUE = 2;
 
     /**
-     * An array of all the '<em><b>Enrich Target Action</b></em>' enumerators.
-     * <!-- begin-user-doc -->
+	 * The '<em><b>REMOVE</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>REMOVE</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #REMOVE
+	 * @model literal="remove"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int REMOVE_VALUE = 3;
+
+				/**
+	 * An array of all the '<em><b>Enrich Target Action</b></em>' enumerators.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     private static final EnrichTargetAction[] VALUES_ARRAY = new EnrichTargetAction[] {
-            REPLACE,
-            CHILD,
-            SIBLING,
-        };
+			REPLACE,
+			CHILD,
+			SIBLING,
+			REMOVE,
+		};
 
     /**
-     * A public read-only list of all the '<em><b>Enrich Target Action</b></em>' enumerators.
-     * <!-- begin-user-doc -->
+	 * A public read-only list of all the '<em><b>Enrich Target Action</b></em>' enumerators.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public static final List<EnrichTargetAction> VALUES = Collections.unmodifiableList(Arrays.asList(VALUES_ARRAY));
 
     /**
-     * Returns the '<em><b>Enrich Target Action</b></em>' literal with the specified literal value.
-     * <!-- begin-user-doc -->
+	 * Returns the '<em><b>Enrich Target Action</b></em>' literal with the specified literal value.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @param literal the literal.
-     * @return the matching enumerator or <code>null</code>.
-     * @generated
-     */
+	 * @param literal the literal.
+	 * @return the matching enumerator or <code>null</code>.
+	 * @generated
+	 */
     public static EnrichTargetAction get(String literal) {
-        for (int i = 0; i < VALUES_ARRAY.length; ++i) {
-            EnrichTargetAction result = VALUES_ARRAY[i];
-            if (result.toString().equals(literal)) {
-                return result;
-            }
-        }
-        return null;
-    }
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			EnrichTargetAction result = VALUES_ARRAY[i];
+			if (result.toString().equals(literal)) {
+				return result;
+			}
+		}
+		return null;
+	}
 
     /**
-     * Returns the '<em><b>Enrich Target Action</b></em>' literal with the specified name.
-     * <!-- begin-user-doc -->
+	 * Returns the '<em><b>Enrich Target Action</b></em>' literal with the specified name.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @param name the name.
-     * @return the matching enumerator or <code>null</code>.
-     * @generated
-     */
+	 * @param name the name.
+	 * @return the matching enumerator or <code>null</code>.
+	 * @generated
+	 */
     public static EnrichTargetAction getByName(String name) {
-        for (int i = 0; i < VALUES_ARRAY.length; ++i) {
-            EnrichTargetAction result = VALUES_ARRAY[i];
-            if (result.getName().equals(name)) {
-                return result;
-            }
-        }
-        return null;
-    }
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			EnrichTargetAction result = VALUES_ARRAY[i];
+			if (result.getName().equals(name)) {
+				return result;
+			}
+		}
+		return null;
+	}
 
     /**
-     * Returns the '<em><b>Enrich Target Action</b></em>' literal with the specified integer value.
-     * <!-- begin-user-doc -->
+	 * Returns the '<em><b>Enrich Target Action</b></em>' literal with the specified integer value.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @param value the integer value.
-     * @return the matching enumerator or <code>null</code>.
-     * @generated
-     */
+	 * @param value the integer value.
+	 * @return the matching enumerator or <code>null</code>.
+	 * @generated
+	 */
     public static EnrichTargetAction get(int value) {
-        switch (value) {
-            case REPLACE_VALUE: return REPLACE;
-            case CHILD_VALUE: return CHILD;
-            case SIBLING_VALUE: return SIBLING;
-        }
-        return null;
-    }
+		switch (value) {
+			case REPLACE_VALUE: return REPLACE;
+			case CHILD_VALUE: return CHILD;
+			case SIBLING_VALUE: return SIBLING;
+			case REMOVE_VALUE: return REMOVE;
+		}
+		return null;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     private final int value;
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     private final String name;
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     private final String literal;
 
     /**
-     * Only this class can construct instances.
-     * <!-- begin-user-doc -->
+	 * Only this class can construct instances.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     private EnrichTargetAction(int value, String name, String literal) {
-        this.value = value;
-        this.name = name;
-        this.literal = literal;
-    }
+		this.value = value;
+		this.name = name;
+		this.literal = literal;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public int getValue() {
-      return value;
-    }
+	  return value;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public String getName() {
-      return name;
-    }
+	  return name;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public String getLiteral() {
-      return literal;
-    }
+	  return literal;
+	}
 
     /**
-     * Returns the literal value of the enumerator, which is its string representation.
-     * <!-- begin-user-doc -->
+	 * Returns the literal value of the enumerator, which is its string representation.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
 
     @Override
     public String toString() {
-        return literal;
-    }
+		return literal;
+	}
 
 } // EnrichTargetAction

2c26b5033a2aa26367bd84f5b6b0d3951d647067
Sajitha Liyanage <sajithaliyanage@gmail.com>
2020-11-19 13:35:42 +0530

    Revert "Add jsonpath support in DBReport mediator"

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/AbstractDBMediatorTransformer.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/AbstractDBMediatorTransformer.java
@@ -23,7 +23,6 @@ import javax.xml.namespace.QName;
 import org.apache.axiom.om.OMElement;
 import org.apache.commons.lang.StringUtils;
 import org.apache.synapse.commons.datasource.DataSourceInformation;
-import org.apache.synapse.config.xml.SynapsePath;
 import org.apache.synapse.config.xml.XMLConfigConstants;
 import org.apache.synapse.endpoints.Endpoint;
 import org.eclipse.emf.ecore.EObject;
@@ -45,7 +44,6 @@ import org.wso2.developerstudio.eclipse.logging.core.Logger;
 import org.wso2.securevault.secret.SecretInformation;
 import org.apache.synapse.mediators.db.AbstractDBMediator;
 import org.apache.synapse.mediators.db.Statement;
-import org.apache.synapse.util.xpath.SynapseJsonPath;
 import org.apache.synapse.util.xpath.SynapseXPath;
 
 /**
@@ -193,14 +191,9 @@ public abstract class AbstractDBMediatorTransformer extends AbstractEsbNodeTrans
             for (SqlParameterDefinition param : sqlStatement.getParameters()) {
                 if (param.getValueType() == SqlParameterValueType.EXPRESSION) {
                     NamespacedProperty namespacedProperty = param.getValueExpression();
-                    SynapsePath expression = null;
+                    SynapseXPath expression = null;
                     try {
-                        String pathExpression = namespacedProperty.getPropertyValue();
-                        if (pathExpression.startsWith("json-eval(")) {
-                            expression = new SynapseJsonPath(pathExpression.substring(10, pathExpression.length() - 1));
-                        } else {
-                            expression = new SynapseXPath(namespacedProperty.getPropertyValue());
-                        }
+                        expression = new SynapseXPath(namespacedProperty.getPropertyValue());
                         for (Entry<String, String> entry : namespacedProperty.getNamespaces().entrySet()) {
                             expression.addNamespace(entry.getKey(), entry.getValue());
                         }

bde27d50d233073ac21db50ed4b66354ec3e4e5a
Rosen Silva <rosen@live.com>
2020-12-07 20:04:15 +0530

    fix issue with undo/redo not working in eclipse 2020

--- plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/part/EsbDiagramActionBarContributor.java
+++ plugins/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/part/EsbDiagramActionBarContributor.java
@@ -1,20 +1,36 @@
 package org.wso2.developerstudio.eclipse.gmf.esb.diagram.part;
 
+import org.eclipse.emf.validation.internal.util.Log;
+import org.eclipse.gef.ui.actions.ActionRegistry;
 import org.eclipse.gmf.runtime.common.ui.action.global.GlobalActionId;
+import org.eclipse.gmf.runtime.common.ui.services.action.contributionitem.ContributionItemService;
+import org.eclipse.gmf.runtime.common.ui.util.IWorkbenchPartDescriptor;
+import org.eclipse.gmf.runtime.common.ui.util.WorkbenchPartDescriptor;
 import org.eclipse.gmf.runtime.diagram.ui.actions.ActionIds;
 import org.eclipse.gmf.runtime.diagram.ui.internal.actions.ZoomContributionItem;
-import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramActionBarContributor;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.part.MultiPageEditorActionBarContributor;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.ui.texteditor.ITextEditorActionConstants;
+import org.wso2.developerstudio.eclipse.gmf.esb.diagram.Activator;
+import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
+import org.wso2.developerstudio.eclipse.logging.core.Logger;
 
 /**
  * @generated
  */
-public class EsbDiagramActionBarContributor extends DiagramActionBarContributor {
+public class EsbDiagramActionBarContributor extends MultiPageEditorActionBarContributor {
+
+	private IWorkbenchPartDescriptor descriptor;
+	private static IDeveloperStudioLog log = Logger.getLog(Activator.PLUGIN_ID);
 
     /**
      * @generated
@@ -34,8 +50,14 @@ public class EsbDiagramActionBarContributor extends DiagramActionBarContributor 
      * @generated NOT
      */
     public void init(IActionBars bars, IWorkbenchPage page) {
-        super.init(bars, page);
-        // registering global save action
+		super.init(bars, page);
+		descriptor = new WorkbenchPartDescriptor(getEditorId(), getEditorClass(), getPage());
+		try {
+			ContributionItemService.getInstance().contributeToActionBars(bars, descriptor);
+		} catch (Exception e) {
+			log.error("Error while creating diagram action items", e);
+		}
+		bars.updateActionBars();
         bars.setGlobalActionHandler(GlobalActionId.SAVE, null);
         // print preview
         IMenuManager fileMenu = bars.getMenuManager().findMenuUsingPath(IWorkbenchActionConstants.M_FILE);
@@ -57,4 +79,32 @@ public class EsbDiagramActionBarContributor extends DiagramActionBarContributor 
             }
         }
     }
+
+	@Override
+	public void setActivePage(IEditorPart activeEditor) {
+		IActionBars actionBars = getActionBars();
+		if (actionBars != null) {
+			if (activeEditor instanceof ITextEditor) {
+				ITextEditor editor = (activeEditor instanceof ITextEditor) ? (ITextEditor) activeEditor : null;
+				actionBars.setGlobalActionHandler(ActionFactory.UNDO.getId(),
+						getTextAction(editor, ITextEditorActionConstants.UNDO));
+				actionBars.setGlobalActionHandler(ActionFactory.REDO.getId(),
+						getTextAction(editor, ITextEditorActionConstants.REDO));
+				actionBars.setGlobalActionHandler(ActionFactory.SELECT_ALL.getId(),
+						getTextAction(editor, ITextEditorActionConstants.SELECT_ALL));
+				actionBars.setGlobalActionHandler(ActionFactory.CUT.getId(),
+						getTextAction(editor, ITextEditorActionConstants.CUT));
+				actionBars.setGlobalActionHandler(ActionFactory.COPY.getId(),
+						getTextAction(editor, ITextEditorActionConstants.COPY));
+				actionBars.setGlobalActionHandler(ActionFactory.PASTE.getId(),
+						getTextAction(editor, ITextEditorActionConstants.PASTE));
+				actionBars.updateActionBars();
+			}
+		}
+	}
+
+	private static IAction getTextAction(ITextEditor editor, String actionID) {
+		return editor == null ? null : editor.getAction(actionID);
+	}
+
 }

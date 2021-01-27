27bcf8cbfa517ef94b516814ae317ffc533550a6
Rosen Silva <rosen@live.com>
2021-01-27 11:01:39 +0530

    Make buitin mi runtime upgradable as seperate p2

--- components/studio-platform/plugins/org.wso2.integrationstudio.distribution.project/src/org/wso2/integrationstudio/distribution/project/publisher/CAppProjectPublisher.java
+++ components/studio-platform/plugins/org.wso2.integrationstudio.distribution.project/src/org/wso2/integrationstudio/distribution/project/publisher/CAppProjectPublisher.java
@@ -52,6 +52,7 @@ public class CAppProjectPublisher implements ICarbonServerModulePublisher {
     private static final String MI_PLUGIN_ID = "org.wso2.integrationstudio.carbon.server44microei";
     private static final String MI_110_PLUGIN_ID = "org.wso2.integrationstudio.carbon.server44microei11";
     private static final String MI_120_PLUGIN_ID = "org.wso2.integrationstudio.carbon.server44microei12";
+    private static final String MI_400_PLUGIN_ID = "org.wso2.integrationstudio.carbon.server44microei40";
     private static final String MI_CAPP_PATH = File.separator + "repository" + File.separator + "deployment"
             + File.separator + "server" + File.separator + "carbonapps";
 
@@ -177,9 +178,9 @@ public class CAppProjectPublisher implements ICarbonServerModulePublisher {
             }
 
             boolean hotDeploymentEnabled = true;
-            boolean isMI120Server = server.getServerType().getId().equals(MI_120_PLUGIN_ID);
+            boolean isMI400Server = server.getServerType().getId().equals(MI_400_PLUGIN_ID);
 
-            if (isMI120Server) {
+            if (isMI400Server) {
                 String carbonHomePath = CarbonServerManager.getServerHome(server).toOSString();
                 String tomlFilePath = FileUtils.addNodesToPath(carbonHomePath,
                         new String[] { "conf", "deployment.toml" });
@@ -193,13 +194,14 @@ public class CAppProjectPublisher implements ICarbonServerModulePublisher {
 
             if (server.getServerType().getId().equals(MI_PLUGIN_ID)
                     || server.getServerType().getId().equals(MI_110_PLUGIN_ID)
-                    || (isMI120Server && !hotDeploymentEnabled)) {
+                    || (server.getServerType().getId().equals(MI_120_PLUGIN_ID) && !hotDeploymentEnabled)
+                    || (isMI400Server && !hotDeploymentEnabled)) {
                 IPath iPath = CarbonServerManager.getServerHome(server);
                 File file = iPath.toFile();
                 copyCApp(file.getAbsolutePath(), project);
                 server.restart("run", new NullProgressMonitor());
 
-            } else if (isMI120Server) {
+            } else if (isMI400Server) {
                 final MICarPublisher runnable = new MICarPublisher(deployLocation, project, server);
                 runnable.run();
                 if (runnable.getException() != null) {
@@ -272,7 +274,7 @@ public class CAppProjectPublisher implements ICarbonServerModulePublisher {
 //				cappName = carFile.getName().substring(0, carFile.getName().length()-4);
 //			}
 			if (server.getServerType().getId().equals(MI_PLUGIN_ID) || server.getServerType().getId().equals(MI_110_PLUGIN_ID) 
-			        || server.getServerType().getId().equals(MI_120_PLUGIN_ID)) {
+			        || server.getServerType().getId().equals(MI_120_PLUGIN_ID) || server.getServerType().getId().equals(MI_400_PLUGIN_ID)) {
 				IPath iPath = CarbonServerManager.getServerHome(server);
 				File file = iPath.toFile();
 				deleteCApp(file.getAbsolutePath(), project);

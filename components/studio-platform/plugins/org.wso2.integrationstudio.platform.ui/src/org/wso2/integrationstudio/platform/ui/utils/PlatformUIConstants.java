008565af7d903a6bafadee2b457a82425e305d5c
Lahiru Madushanka <gdlmadushanka@gmail.com>
2024-07-17 20:44:44 +0530

    Prepare to 8.3.0 release
    Update 8.3.0 release with MI 4.3.0 configs

--- components/studio-platform/plugins/org.wso2.integrationstudio.platform.ui/src/org/wso2/integrationstudio/platform/ui/utils/PlatformUIConstants.java
+++ components/studio-platform/plugins/org.wso2.integrationstudio.platform.ui/src/org/wso2/integrationstudio/platform/ui/utils/PlatformUIConstants.java
@@ -39,6 +39,6 @@ public class PlatformUIConstants {
     public static final String DOCKER_TAG_REGEX = "[\\w][\\w.-]{0,127}";
     public static final String DOCKER_DEFAULT_BASE_REPOSITORY = "docker.wso2.com/wso2mi";
     public static final String DOCKER_DEFAULT_BASE_TAG = "4.3.0";
-    public static final String MI_DEPLOYMENT_TOML_TEMPLATE_VERSION = "4.3.0-beta";
-    public static final String EMBEDDED_RUNTIME_DEFAULT_VERSION = "4.3.0-beta";
+    public static final String MI_DEPLOYMENT_TOML_TEMPLATE_VERSION = "4.3.0";
+    public static final String EMBEDDED_RUNTIME_DEFAULT_VERSION = "4.3.0";
 }
